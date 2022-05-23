33ee6ea65e3763d117d176bfc25f440fb695fb20
Patrick José Pereira <patrickelectric@gmail.com>
2022-05-23 14:06:10 -0300

    libs: connection: Add ConnectionManager
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/libs/connection/connection-manager.ts
@@ -0,0 +1,42 @@
+import * as Vehicle from '@/libs/vehicle/protocol/protocol'
+
+import * as Connection from './connection'
+import { WebSocketConnection } from './websocket-connection'
+
+/**
+ *
+ */
+export class ConnectionManager {
+  private static _self: ConnectionManager
+  private static _connections: Connection.Abstract[] = []
+
+  /**
+   * Return the connections available
+   *
+   * @returns {Connection.Abstract[]}
+   */
+  static connections(): Connection.Abstract[] {
+    return ConnectionManager._connections
+  }
+
+  /**
+   * Add a specific connection
+   *
+   * @param  {Connection.Scheme} scheme
+   * @param  {Vehicle.ProtocolType} vehicleProtocol
+   */
+  static addConnection(
+    scheme: Connection.Scheme,
+    vehicleProtocol: Vehicle.ProtocolType
+  ): void {
+    switch (scheme.type()) {
+      case Connection.Type.WebSocket:
+        ConnectionManager._connections.push(
+          new WebSocketConnection(scheme, vehicleProtocol)
+        )
+        break
+      default:
+        unimplemented(`connection type not supported: ${scheme.type()}`)
+    }
+  }
+}
