01d5934ea970e0857eff9850546267b6b87c7db3
Patrick José Pereira <patrickelectric@gmail.com>
2022-05-23 14:02:57 -0300

    main: Add example
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,16 @@ import router from './router'
 loadFonts()
 
 createApp(App).use(router).use(vuetify).use(createPinia()).mount('#app')
+
+import * as Connection from '@/libs/connection/connection'
+import { ConnectionManager } from '@/libs/connection/connection-manager'
+import * as Protocol from '@/libs/vehicle/protocol/protocol'
+
+ConnectionManager.addConnection(
+  new Connection.Scheme('ws://0.0.0.0:8088/ws/mavlink'),
+  Protocol.ProtocolType.MAVLink
+)
+
+ConnectionManager.connections().forEach((connection) =>
+  connection.onRead((message) => console.log(message))
+)
