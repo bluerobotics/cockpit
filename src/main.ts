dc5730f777a8517027cb4f977c4f0c42220ea492
Patrick José Pereira <patrickelectric@gmail.com>
2022-05-25 09:44:59 -0300

    main: Create vehicle
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ createApp(App).use(router).use(vuetify).use(createPinia()).mount('#app')
 import * as Connection from '@/libs/connection/connection'
 import { ConnectionManager } from '@/libs/connection/connection-manager'
 import * as Protocol from '@/libs/vehicle/protocol/protocol'
+import * as Vehicle from '@/libs/vehicle/vehicle'
+import { VehicleFactory } from '@/libs/vehicle/vehicle-factory'
 
 ConnectionManager.addConnection(
   new Connection.Scheme('ws://0.0.0.0:8088/ws/mavlink'),
@@ -20,3 +22,10 @@ ConnectionManager.addConnection(
 )
 
 ConnectionManager.mainConnection()?.onRead((message) => console.log(message))
+
+const vehicle = VehicleFactory.createVehicle(
+  Vehicle.Firmware.ArduPilot,
+  Vehicle.Type.Sub
+)
+
+console.log(vehicle)
