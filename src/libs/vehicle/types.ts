9c595d3275ea25f46b92794c785cf61d5a80f324
Patrick José Pereira <patrickelectric@gmail.com>
2022-05-25 09:43:08 -0300

    libs: vehicle: types: Add Partial constructor and helper functions
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/libs/vehicle/types.ts
+++ src/libs/vehicle/types.ts
@@ -7,6 +7,66 @@ export class Coordinates {
   altitudeAccuracy: number // Should be between 0 and 1
   latitude: number // Should be in de decimal degrees. E.g: -27.593500
   longitude: number // Should be in de decimal degrees. E.g: -48.558540
+
+  /**
+   * Create object
+   *
+   * @param {Partial<Coordinates>} init
+   */
+  public constructor(init?: Partial<Coordinates>) {
+    Object.assign(this, init)
+  }
+
+  /**
+   *
+   * @param {number} latitude
+   * @param {number} longitude
+   * @param {number} accuracy
+   * @returns {Coordinates}
+   */
+  setPosition(
+    latitude: number,
+    longitude: number,
+    accuracy?: number
+  ): Coordinates {
+    this.latitude = latitude
+    this.longitude = longitude
+
+    if (accuracy !== undefined) {
+      if (accuracy < 0 || accuracy > 1) {
+        console.error(`accuracy outside of valid range: ${accuracy}`)
+        return this
+      }
+      this.accuracy = accuracy
+    }
+
+    return this
+  }
+
+  /**
+   * Set altitude information
+   *
+   * @param  {number?} altitude
+   * @param  {number?} altitudeAccuracy
+   * @returns {Coordinates}
+   */
+  setAltitude(altitude?: number, altitudeAccuracy?: number): Coordinates {
+    if (altitude !== undefined) {
+      this.altitude = altitude
+    }
+
+    if (altitudeAccuracy !== undefined) {
+      if (altitudeAccuracy < 0 || altitudeAccuracy > 1) {
+        console.error(
+          `altitudeAccuracy outside of valid range: ${altitudeAccuracy}`
+        )
+        return this
+      }
+      this.altitudeAccuracy = altitudeAccuracy
+    }
+
+    return this
+  }
 }
 
 /**
@@ -16,6 +76,15 @@ export class Attitude {
   roll: number
   pitch: number
   yaw: number
+
+  /**
+   * Create object
+   *
+   * @param {Partial<Attitude>} init
+   */
+  public constructor(init?: Partial<Attitude>) {
+    Object.assign(this, init)
+  }
 }
 
 /**
@@ -24,6 +93,15 @@ export class Attitude {
 export class Battery {
   cells: number[] // List of cell voltage in volts
   voltage: number // In volts
+
+  /**
+   * Create object
+   *
+   * @param {Partial<Attitude>} init
+   */
+  public constructor(init?: Partial<Battery>) {
+    Object.assign(this, init)
+  }
 
   /**
    * Number of cells in the battery
