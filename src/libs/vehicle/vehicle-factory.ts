cee70b821ee9e82c8f376777cd64488a59938e86
Patrick José Pereira <patrickelectric@gmail.com>
2022-05-25 09:43:41 -0300

    libs: vehicle: Improve factory to create ArduPilotSub and register all vehicles
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/libs/vehicle/vehicle-factory.ts
+++ src/libs/vehicle/vehicle-factory.ts
@@ -1,39 +1,67 @@
 //import * as Protocol from './protocol/protocol'
+import { ArduPilotSub } from './ardupilot/ardusub'
 import * as Vehicle from './vehicle'
 
 /**
- * Create vehicle based on the firmware and vehicle type
  *
- * @param {Vehicle.Firmware} firmware
- * @param {Vehicle.Type} type
  */
-export function createVehicle(
-  firmware: Vehicle.Firmware,
-  type: Vehicle.Type
-): void {
-  unused(type)
-  switch (firmware) {
-    case Vehicle.Firmware.ArduPilot:
-      unimplemented()
-      //return createArduPilotVehicle(type)
-      break
-    default:
-      unimplemented('Firmware not supported')
-  }
-}
+export class VehicleFactory {
+  static _vehicles: WeakRef<Vehicle.Abstract>[] = []
 
-/**
- * Create ArduPilot vehicle based on the category
- *
- * @param  {Vehicle.Type} type
- */
-export function createArduPilotVehicle(type: Vehicle.Type): void {
-  switch (type) {
-    case Vehicle.Type.Sub:
-      unimplemented()
-      //return new ArduPilotSub()
-      break
-    default:
+  /**
+   * Create vehicle based on the firmware and vehicle type
+   *
+   * @param {Vehicle.Firmware} firmware
+   * @param {Vehicle.Type} type
+   * @returns {Vehicle.Abstract | undefined}
+   */
+  static createVehicle(
+    firmware: Vehicle.Firmware,
+    type: Vehicle.Type
+  ): Vehicle.Abstract | undefined {
+    let vehicle = undefined
+
+    switch (firmware) {
+      case Vehicle.Firmware.ArduPilot:
+        vehicle = VehicleFactory.createArduPilotVehicle(type)
+        break
+    }
+
+    if (vehicle == undefined) {
       unimplemented('Firmware not supported')
+    }
+
+    VehicleFactory._vehicles.push(new WeakRef(vehicle))
+    return vehicle
+  }
+
+  /**
+   * Create ArduPilot vehicle based on the category
+   *
+   * @param  {Vehicle.Type} type
+   * @returns {Vehicle.Abstract | undefined}
+   */
+  static createArduPilotVehicle(
+    type: Vehicle.Type
+  ): Vehicle.Abstract | undefined {
+    switch (type) {
+      case Vehicle.Type.Sub:
+        return new ArduPilotSub()
+      default:
+        unimplemented('Firmware not supported')
+    }
+  }
+  /**
+   * Return a list of vehicles available
+   *
+   * @returns {WeakRef<Vehicle.Abstract>[]}
+   */
+  vehicles(): WeakRef<Vehicle.Abstract>[] {
+    // Be sure to remove vehicles that does not exist anymore
+    VehicleFactory._vehicles = VehicleFactory._vehicles.filter(
+      (weakRef) => weakRef.deref() !== undefined
+    )
+
+    return VehicleFactory._vehicles
   }
 }
