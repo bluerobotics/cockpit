2936bb158db9844d7dbc03eaf54e30b80e8581e9
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-08-22 16:28:03 -0300

    use video samples from submodule

--- src/components/widgets/VideoPlayer.vue
+++ src/components/widgets/VideoPlayer.vue
@@ -1,9 +1,22 @@
 <template>
   <video muted loop autoplay>
-    <source src="@/assets/bbb.mp4" type="video/mp4" disablePictureInPicture />
+    <source :src="videoSource" type="video/mp4" disablePictureInPicture />
     Your browser does not support the video tag.
   </video>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const videoSource = ref('')
+const sampleVideos = [
+  '/src/assets/sample-videos/Big-Buck-Bunny/ElephantsDream.mp4',
+  '/src/assets/sample-videos/Big-Buck-Bunny/BigBuckBunny1080p30s.mp4',
+]
+videoSource.value =
+  sampleVideos[Math.floor(Math.random() * sampleVideos.length)]
+console.log(videoSource.value)
+</script>
 
 <style scoped>
 video {
