80ceb7653860acb8848639a5b81ae4a015233c80
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-08-24 16:00:58 -0300

    new-widget: `Map`
    The `Map` widget is used for general mapping functionality.
    For the beggining, it just displays a map, but it can be extended to show information like markers.

---
+++ src/components/widgets/Map.vue
@@ -0,0 +1,28 @@
+<template>
+  <l-map
+    v-model="zoom"
+    v-model:zoom="zoom"
+    v-model:bounds="bounds"
+    v-model:center="center"
+    class="map"
+  >
+    <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+  </l-map>
+</template>
+
+<script setup lang="ts">
+import 'leaflet/dist/leaflet.css'
+
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import { ref } from 'vue'
+
+const zoom = ref(11)
+const bounds = ref(null)
+const center = ref([-27.5935, -48.55854])
+</script>
+
+<style scoped>
+.map {
+  z-index: 0;
+}
+</style>
