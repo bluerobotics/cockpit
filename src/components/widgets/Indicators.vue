7c647334562ad39e7c398dbc61a3ec27541154bd
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-09-01 17:26:30 -0300

    widget[indicators]: Use real data from `mainVehicle` store

--- src/components/widgets/Indicators.vue
+++ src/components/widgets/Indicators.vue
@@ -1,20 +1,28 @@
 <template>
   <v-sheet rounded color="rgba(255, 255, 255, 0.9)" class="indications">
-    <p class="font-weight-bold text-body-1">Speed:</p>
-    <p class="text-body-1">{{ store.speed }} km/h</p>
-    <p class="font-weight-bold text-body-1">Heading:</p>
-    <p class="text-body-1">{{ store.heading }} deg</p>
-    <p class="font-weight-bold text-body-1">Depth:</p>
-    <p class="text-body-1">{{ store.depth }} m</p>
-    <p class="font-weight-bold text-body-1">Water temp:</p>
-    <p class="text-body-1">{{ store.waterTemp }} oC</p>
+    <p class="font-weight-bold text-body-1">Vehicle online?</p>
+    <p class="text-body-1">{{ store.isVehicleOnline() }}</p>
+    <p class="font-weight-bold text-body-1">Firmware:</p>
+    <p class="text-body-1">{{ store.firmwareType }}</p>
+    <p class="font-weight-bold text-body-1">Vehicle:</p>
+    <p class="text-body-1">{{ store.vehicleType }}</p>
+    <p class="font-weight-bold text-body-1">Lat/Long:</p>
+    <p class="text-body-1">
+      {{ store.coordinates?.latitude }}/{{ store.coordinates?.longitude }}
+    </p>
+    <p class="font-weight-bold text-body-1">Pitch:</p>
+    <p class="text-body-1">{{ degrees(store.attitude?.pitch) }} deg</p>
+    <p class="font-weight-bold text-body-1">Roll:</p>
+    <p class="text-body-1">{{ degrees(store.attitude?.roll) }} deg</p>
+    <p class="font-weight-bold text-body-1">Yaw:</p>
+    <p class="text-body-1">{{ degrees(store.attitude?.yaw) }} deg</p>
   </v-sheet>
 </template>
 
 <script setup lang="ts">
-import { useBoatStore } from '@/stores/boat'
-
-const store = useBoatStore()
+import { degrees } from '@/libs/utils'
+import { useMainVehicleStore } from '@/stores/mainVehicle'
+const store = useMainVehicleStore()
 </script>
 
 <style scoped>
