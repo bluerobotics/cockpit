873e94823d70ad70d2a7a2111ccfb716e7677890
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-09-01 17:24:26 -0300

    utils: Add functions for converting between degrees and radians

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,7 @@
 export const constrain = (value: number, min: number, max: number): number => {
   return Math.max(Math.min(value, max), min)
 }
+
+export const radians = (angle: number): number => (angle * Math.PI) / 180
+
+export const degrees = (angle: number): number => (angle * 180) / Math.PI
