8d4a9d285e85ac1f8cfd6ac718fa56b26aa40fac
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-09-12 14:25:15 -0300

    src: Allow `Indicators` widget to be configured

--- src/components/widgets/Indicators.vue
+++ src/components/widgets/Indicators.vue
@@ -1,28 +1,88 @@
 <template>
   <v-sheet rounded color="rgba(255, 255, 255, 0.9)" class="indications">
-    <p class="font-weight-bold text-body-1">Vehicle online?</p>
-    <p class="text-body-1">{{ store.isVehicleOnline() }}</p>
-    <p class="font-weight-bold text-body-1">Firmware:</p>
-    <p class="text-body-1">{{ store.firmwareType }}</p>
-    <p class="font-weight-bold text-body-1">Vehicle:</p>
-    <p class="text-body-1">{{ store.vehicleType }}</p>
-    <p class="font-weight-bold text-body-1">Lat/Long:</p>
-    <p class="text-body-1">
-      {{ store.coordinates?.latitude }}/{{ store.coordinates?.longitude }}
-    </p>
-    <p class="font-weight-bold text-body-1">Pitch:</p>
-    <p class="text-body-1">{{ degrees(store.attitude?.pitch) }} deg</p>
-    <p class="font-weight-bold text-body-1">Roll:</p>
-    <p class="text-body-1">{{ degrees(store.attitude?.roll) }} deg</p>
-    <p class="font-weight-bold text-body-1">Yaw:</p>
-    <p class="text-body-1">{{ degrees(store.attitude?.yaw) }} deg</p>
+    <v-btn
+      class="options-btn"
+      icon="mdi-dots-vertical"
+      size="x-small"
+      variant="text"
+      flat
+      @click="showOptionsDialog = !showOptionsDialog"
+    />
+    <template v-if="widget.options.showDebugInfo">
+      <p class="font-weight-bold text-body-1">Vehicle online?</p>
+      <p class="text-body-1">{{ store.isVehicleOnline() }}</p>
+      <p class="font-weight-bold text-body-1">Firmware:</p>
+      <p class="text-body-1">{{ store.firmwareType }}</p>
+      <p class="font-weight-bold text-body-1">Vehicle:</p>
+      <p class="text-body-1">{{ store.vehicleType }}</p>
+    </template>
+    <template v-if="widget.options.showCoordinates">
+      <p class="font-weight-bold text-body-1">Lat/Long:</p>
+      <p class="text-body-1">
+        {{ store.coordinates?.latitude }}/{{ store.coordinates?.longitude }}
+      </p>
+    </template>
+    <template v-if="widget.options.showAttitude">
+      <p class="font-weight-bold text-body-1">Pitch:</p>
+      <p class="text-body-1">{{ degrees(store.attitude?.pitch) }} deg</p>
+      <p class="font-weight-bold text-body-1">Roll:</p>
+      <p class="text-body-1">{{ degrees(store.attitude?.roll) }} deg</p>
+      <p class="font-weight-bold text-body-1">Yaw:</p>
+      <p class="text-body-1">{{ degrees(store.attitude?.yaw) }} deg</p>
+    </template>
   </v-sheet>
+  <v-dialog v-model="showOptionsDialog">
+    <v-card class="pa-2">
+      <v-card-title>Indicators widget config</v-card-title>
+      <v-card-text>
+        <v-checkbox
+          v-model="widget.options.showDebugInfo"
+          label="showDebugInfo"
+          hide-details
+        />
+        <v-checkbox
+          v-model="widget.options.showCoordinates"
+          label="showCoordinates"
+          hide-details
+        />
+        <v-checkbox
+          v-model="widget.options.showAttitude"
+          label="showAttitude"
+          hide-details
+        />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref, toRefs } from 'vue'
+
 import { degrees } from '@/libs/utils'
 import { useMainVehicleStore } from '@/stores/mainVehicle'
+import type { Widget } from '@/types/widgets'
+
 const store = useMainVehicleStore()
+const props = defineProps<{
+  /**
+   * Widget reference
+   */
+  widget: Widget
+}>()
+
+const widget = toRefs(props).widget
+const showOptionsDialog = ref(false)
+
+onMounted(() => {
+  // Set initial widget options if they don't exist
+  if (Object.keys(widget.value.options).length === 0) {
+    widget.value.options = {
+      showCoordinates: true,
+      showAttitude: true,
+      showDebugInfo: true,
+    }
+  }
+})
 </script>
 
 <style scoped>
@@ -33,5 +93,11 @@ const store = useMainVehicleStore()
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+.options-btn {
+  position: absolute;
+  margin: 5px;
+  top: 0;
+  right: 0;
 }
 </style>
