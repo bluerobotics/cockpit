07aa2a674b49e229ac57c64cfdc7dcb14ba4739b
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-09-22 15:22:46 -0300

    src: utils: Add object comparison function

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export const constrain = (value: number, min: number, max: number): number => {
   return Math.max(Math.min(value, max), min)
 }
@@ -5,3 +7,7 @@ export const constrain = (value: number, min: number, max: number): number => {
 export const radians = (angle: number): number => (angle * Math.PI) / 180
 
 export const degrees = (angle: number): number => (angle * 180) / Math.PI
+
+export const isEqual = (obj1: any, obj2: any): boolean => {
+  return JSON.stringify(obj1) === JSON.stringify(obj2)
+}
