84bffa28b09ca66ec1b65487dccfb7fc3698fe94
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-11-08 18:49:08 -0300

    configuration-menu: Add initial configuration menu views

---
+++ src/views/ConfigurationGeneralView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main">
+    <h1>General configuration</h1>
+  </div>
+</template>
+
+<style>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
