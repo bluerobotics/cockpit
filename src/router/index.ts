66c1ab3da5066d84d91dd5dd9b7bcfade3d9ae56
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-11-08 17:04:11 -0300

    routes: Stop lazy-loading views
    We want the application fully loaded on start to improve UX.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import {
   createWebHistory,
 } from 'vue-router'
 
+import AboutViewVue from '../views/AboutView.vue'
 import WidgetsView from '../views/WidgetsView.vue'
 
 const router = createRouter({
@@ -19,10 +20,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: AboutViewVue,
     },
   ],
 })
