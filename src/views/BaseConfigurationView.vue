5dceeb78a79d19e6348120655a8d1dc23b06c44d
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-12-14 11:27:58 -0300

    configuration-view: Create base template component

---
+++ src/views/BaseConfigurationView.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card class="elevation-0">
+    <v-card-title class="flex-centered mt-2">
+      <slot name="title"></slot>
+    </v-card-title>
+    <v-divider />
+    <v-card-text class="flex-centered">
+      <slot name="content"></slot>
+    </v-card-text>
+  </v-card>
+</template>
