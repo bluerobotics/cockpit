d528d9bbf8aed95d6d6714f9a953b9c6ec620ed2
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-01-30 11:57:31 -0300

    app: Remove unused code from `App` component

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,6 @@
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App',
-
-  data() {
-    return {
-      //
-    }
-  },
-})
-</script>
-
 <style>
 html {
   /* Removes the scrollbar */
