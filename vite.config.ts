65bcb41f042b9ba7f1cdc208271f412ab9a53e68
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-02-03 16:27:48 -0300

    pwa: Add initial configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import electron from 'vite-plugin-electron'
+import { VitePWA } from 'vite-plugin-pwa'
 import vuetify from 'vite-plugin-vuetify'
 
 const path = require('path') // eslint-disable-line @typescript-eslint/no-var-requires
@@ -15,6 +16,32 @@ export default defineConfig({
     vue(),
     vuetify({
       autoImport: true,
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Cocopit',
+        short_name: 'Cocopit',
+        description: `An intuitive and customizable cross-platform ground control
+          station for remote vehicles of all types.`,
+        theme_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'landscape',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
     }),
   ],
   define: { 'process.env': {} },
