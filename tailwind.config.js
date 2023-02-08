40e17fc7be83c7b80e78539a22209e0dcc34e8d2
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-02-08 17:32:31 -0300

    config: tailwindcss: Add custom `critical` color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
+    colors: {
+      critical: '#7F34CF',
+    },
     extend: {},
   },
   plugins: [],
