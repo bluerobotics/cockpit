7337cc8381b41df743aca00169b0ca0ca47cad7c
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-02-08 15:45:47 -0300

    packages: Add `tailwindcss`

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
