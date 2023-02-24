b330c84dd4bcf030940ff2bb2f32bf5d545db5f9
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-02-24 14:30:18 -0300

    packages: Remove `cypress` (end-to-end testing)
    We are currently not using it and it often brakes the CI for obscure reasons.
    
    When needed, whe can bring it back.

--- cypress/plugins/index.ts
+++
@@ -1,5 +0,0 @@
-export default ((on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  return config
-}) as Cypress.PluginConfig
