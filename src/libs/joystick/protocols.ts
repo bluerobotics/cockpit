52dbdee30eb08089d4be67e1718cff6f49bb36aa
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-03-13 17:42:58 -0300

    joystick: Provice direct access to joystick standard mapped state

--- src/libs/joystick/protocols.ts
+++ src/libs/joystick/protocols.ts
@@ -1,7 +1,12 @@
 import { cockpitStandardToMavlink } from '@/assets/joystick-profiles'
 import { round, scale } from '@/libs/utils'
 import { sequentialArray } from '@/libs/utils'
-import { type ProtocolControllerMapping, JoystickProtocol, ProtocolControllerState } from '@/types/joystick'
+import {
+  type JoystickState,
+  type ProtocolControllerMapping,
+  JoystickProtocol,
+  ProtocolControllerState,
+} from '@/types/joystick'
 
 export type MavlinkControllerMapping = ProtocolControllerMapping
 
@@ -19,20 +24,19 @@ export class MavlinkControllerState extends ProtocolControllerState {
 
   /**
    *
-   * @param { Gamepad } gamepad - Gamepad object where the joystick state is stored.
+   * @param { JoystickState } joystickState - Cockpit standard mapped values for the joystick
    * @param { MavlinkControllerMapping } mapping - Gamepad API to Mavlink joystick mapping, where assignments and limits are got from.
    * @param { number } target - Specify targeted vehicle ID.
    */
-  constructor(gamepad: Gamepad, mapping: MavlinkControllerMapping, target = 1) {
+  constructor(joystickState: JoystickState, mapping: MavlinkControllerMapping, target = 1) {
     super()
 
     let buttons_int = 0
     for (let i = 0; i < MavlinkControllerState.BUTTONS_PER_BITFIELD; i++) {
       const gamepadButtonPosition = mapping.buttons.findIndex((v) => v === i)
       if (gamepadButtonPosition === -1) continue
-      const gamepadButtonState = gamepad.buttons[gamepadButtonPosition]
-      if (gamepadButtonState === undefined) continue
-      buttons_int += +gamepadButtonState.pressed * 2 ** i
+      const gamepadButtonState = joystickState.buttons[gamepadButtonPosition]
+      buttons_int += (gamepadButtonState ?? 0) * 2 ** i
     }
 
     const xIndex = mapping.axesCorrespondencies.findIndex((v) => v === 'x')
@@ -40,17 +44,17 @@ export class MavlinkControllerState extends ProtocolControllerState {
     const zIndex = mapping.axesCorrespondencies.findIndex((v) => v === 'z')
     const rIndex = mapping.axesCorrespondencies.findIndex((v) => v === 'r')
 
-    const xLimits = [mapping.axesMins[xIndex], mapping.axesMaxs[xIndex]]
-    const yLimits = [mapping.axesMins[yIndex], mapping.axesMaxs[yIndex]]
-    const zLimits = [mapping.axesMins[zIndex], mapping.axesMaxs[zIndex]]
-    const rLimits = [mapping.axesMins[rIndex], mapping.axesMaxs[rIndex]]
-
     const absLimits = mavlinkAxesLimits
 
-    this.x = round(scale(gamepad.axes[xIndex], -1, 1, xLimits[0] ?? absLimits[0], xLimits[1] ?? absLimits[1]), 0)
-    this.y = round(scale(gamepad.axes[yIndex], -1, 1, yLimits[0] ?? absLimits[0], yLimits[1] ?? absLimits[1]), 0)
-    this.z = round(scale(gamepad.axes[zIndex], -1, 1, zLimits[0] ?? absLimits[0], zLimits[1] ?? absLimits[1]), 0)
-    this.r = round(scale(gamepad.axes[rIndex], -1, 1, rLimits[0] ?? absLimits[0], rLimits[1] ?? absLimits[1]), 0)
+    const xLimits = [mapping.axesMins[xIndex] ?? absLimits[0], mapping.axesMaxs[xIndex] ?? absLimits[1]]
+    const yLimits = [mapping.axesMins[yIndex] ?? absLimits[0], mapping.axesMaxs[yIndex] ?? absLimits[1]]
+    const zLimits = [mapping.axesMins[zIndex] ?? absLimits[0], mapping.axesMaxs[zIndex] ?? absLimits[1]]
+    const rLimits = [mapping.axesMins[rIndex] ?? absLimits[0], mapping.axesMaxs[rIndex] ?? absLimits[1]]
+
+    this.x = xIndex === undefined ? 0 : round(scale(joystickState.axes[xIndex] ?? 0, -1, 1, xLimits[0], xLimits[1]), 0)
+    this.y = yIndex === undefined ? 0 : round(scale(joystickState.axes[yIndex] ?? 0, -1, 1, yLimits[0], yLimits[1]), 0)
+    this.z = zIndex === undefined ? 0 : round(scale(joystickState.axes[zIndex] ?? 0, -1, 1, zLimits[0], zLimits[1]), 0)
+    this.r = rIndex === undefined ? 0 : round(scale(joystickState.axes[rIndex] ?? 0, -1, 1, rLimits[0], rLimits[1]), 0)
 
     this.buttons = buttons_int
     this.target = round(target, 0)
