8ae227b33ddccab8478e7726b64c372aed17c4b3
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-06-06 18:33:35 -0300

    alerter: Show older notifications on interactive window

--- src/components/widgets/Alerter.vue
+++ src/components/widgets/Alerter.vue
@@ -1,16 +1,33 @@
 <template>
-  <div
-    class="flex items-center justify-between flex-grow p-2 mx-1 my-1.5 overflow-hidden rounded whitespace-nowrap bg-slate-800/75"
-  >
-    <p class="mx-1 overflow-hidden text-xl font-medium text-gray-100">{{ currentAlert.message }}</p>
-    <p class="mx-1 text-gray-100">
-      {{ formattedDate(currentAlert.time_created || new Date()) }}
-    </p>
+  <div class="relative flex-grow mx-1 my-1.5">
+    <div
+      class="flex items-center justify-between p-1 overflow-hidden rounded cursor-pointer whitespace-nowrap bg-slate-800/75"
+      @click="toggleExpandedAlerts()"
+    >
+      <p class="mx-1 overflow-hidden text-xl font-medium text-gray-100">{{ currentAlert.message }}</p>
+      <p class="mx-1 text-gray-100">
+        {{ formattedDate(currentAlert.time_created || new Date()) }}
+      </p>
+    </div>
+    <div
+      class="absolute w-full p-2 transition-all rounded top-12 max-h-[30vh] overflow-y-auto text-slate-50 scrollbar-hide bg-slate-800/75 select-none flex flex-col"
+      :class="{ 'opacity-0': !isShowingExpandedAlerts }"
+    >
+      <div v-for="(alert, i) in alertStore.sortedAlerts.reverse()" :key="alert.time_created.toISOString()">
+        <div class="flex items-center justify-between whitespace-nowrap">
+          <p class="mx-1 overflow-hidden text-lg font-medium leading-none text-ellipsis">{{ alert.message }}</p>
+          <p class="mx-1">
+            {{ formattedDate(alert.time_created || new Date()) }}
+          </p>
+        </div>
+        <div v-if="i !== alertStore.alerts.length - 1" class="h-px mx-1 mb-2 bg-slate-50/30" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useTimestamp } from '@vueuse/core'
+import { useTimestamp, useToggle } from '@vueuse/core'
 import { format } from 'date-fns'
 import { computed } from 'vue'
 
@@ -33,4 +50,6 @@ const currentAlert = computed((): Alert => {
   }
   return alertStore.alerts.last()!
 })
+
+const [isShowingExpandedAlerts, toggleExpandedAlerts] = useToggle()
 </script>
