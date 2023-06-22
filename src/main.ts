0af9a6cd71c0a943fc5330ad836c3c65205d46fe
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-06-22 14:03:08 -0300

    packages: Add icons from `font-awesome`

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import 'floating-vue/dist/style.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import FloatingVue from 'floating-vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
@@ -9,8 +13,10 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 
+library.add(fas, far)
 loadFonts()
 
 const app = createApp(App)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.use(router).use(vuetify).use(createPinia()).use(FloatingVue)
 app.mount('#app')
