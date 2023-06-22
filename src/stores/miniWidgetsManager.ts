9e7a406548db4e9282ec2a915660ebdef54c8014
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-06-22 14:03:08 -0300

    defaults: Add default mini-widget profile

--- src/stores/miniWidgetsManager.ts
+++ src/stores/miniWidgetsManager.ts
@@ -4,13 +4,11 @@ import '@/libs/cosmos'
 import { useStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
 
+import { miniWidgetsProfile } from '@/assets/defaults'
 import { type MiniWidget, type MiniWidgetContainer, MiniWidgetType } from '@/types/miniWidgets'
 
 export const useMiniWidgetsManagerStore = defineStore('mini-widgets-manager', () => {
-  const currentMiniWidgetsProfile = useStorage('cockpit-mini-widgets-profile', {
-    name: 'Default Mini Widget Profile',
-    containers: [],
-  })
+  const currentMiniWidgetsProfile = useStorage('cockpit-mini-widgets-profile', miniWidgetsProfile)
 
   function addWidgetToContainer(widgetType: MiniWidgetType, container: MiniWidgetContainer): void {
     container.widgets.unshift({
