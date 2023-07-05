8c419abeecec565dccd0396a3ed6e46324aa79d1
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-07-05 12:40:22 -0300

    alerts: Allow changing alert voice
    There are some voices that are very buggy on some systems. Letting the user choose the voice allows fixing this.

--- src/views/ConfigurationAlertsView.vue
+++ src/views/ConfigurationAlertsView.vue
@@ -13,11 +13,20 @@
           />
         </div>
       </div>
+      <span class="text-sm font-medium text-slate-500">Alert voice:</span>
+      <Dropdown
+        v-model="alertStore.selectedAlertSpeechVoiceName"
+        :options="alertStore.availableAlertSpeechVoiceNames"
+        name-key="name"
+        value-key="value"
+        class="max-w-[200px] my-2"
+      />
     </template>
   </BaseConfigurationView>
 </template>
 
 <script setup lang="ts">
+import Dropdown from '@/components/Dropdown.vue'
 import { useAlertStore } from '@/stores/alert'
 
 import BaseConfigurationView from './BaseConfigurationView.vue'
