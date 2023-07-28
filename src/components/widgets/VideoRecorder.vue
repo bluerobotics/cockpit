0cefc696d0f65ca9de49f7504b13cc32bf0b836c
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-07-28 18:45:53 -0300

    Add mission name and timestamp to VideoRecorder recordings

--- src/components/widgets/VideoRecorder.vue
+++ src/components/widgets/VideoRecorder.vue
@@ -45,6 +45,7 @@
 
 <script setup lang="ts">
 import { useMouseInElement } from '@vueuse/core'
+import { format } from 'date-fns'
 import { saveAs } from 'file-saver'
 import Swal, { type SweetAlertResult } from 'sweetalert2'
 import { computed, onBeforeMount, onBeforeUnmount, ref, toRefs, watch } from 'vue'
@@ -53,9 +54,11 @@ import adapter from 'webrtc-adapter'
 import { WebRTCManager } from '@/composables/webRTC'
 import type { Stream } from '@/libs/webrtc/signalling_protocol'
 import { useMainVehicleStore } from '@/stores/mainVehicle'
+import { useMissionStore } from '@/stores/mission'
 import type { Widget } from '@/types/widgets'
 
 const { rtcConfiguration, webRTCSignallingURI } = useMainVehicleStore()
+const { missionName } = useMissionStore()
 
 console.debug('[WebRTC] Using webrtc-adapter for', adapter.browserDetails)
 
@@ -171,7 +174,7 @@ const startRecording = async (): Promise<SweetAlertResult | void> => {
   mediaRecorder.value.onstop = () => {
     const blob = new Blob(chunks, { type: 'video/webm' })
     chunks = []
-    saveAs(blob, 'video')
+    saveAs(blob, `${missionName} (${format(new Date(), 'LLL dd, yyyy - HH꞉mm꞉ss O')})`)
     mediaRecorder.value = undefined
     if (selectedStream.value?.id === 'screenStream' && mediaStream.value !== undefined) {
       // If recording the screen stream, stop the tracks also, so the browser removes the recording warning.
