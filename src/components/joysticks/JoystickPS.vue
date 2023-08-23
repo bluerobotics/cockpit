e4859ae8edde2ec806c4e36cb4801c8634103e75
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-08-23 17:50:52 -0300

    Reorganize and rename joystick input related types

--- src/components/joysticks/JoystickPS.vue
+++ src/components/joysticks/JoystickPS.vue
@@ -2,63 +2,13 @@
   <object :class="component_name" type="image/svg+xml" :data="joystick_svg_path" />
 </template>
 
-<script lang="ts">
-/**
- * Buttons for PS4 controller
- */
-export enum Button {
-  B0 = 0, // Bottom button in right cluster
-  B1 = 1, // Right button in right cluster
-  B2 = 2, // Left button in right cluster
-  B3 = 3, // Top button in right cluster
-  B4 = 4, // Top left front button
-  B5 = 5, // Top right front button
-  B6 = 6, // Bottom left front button
-  B7 = 7, // Bottom right front button
-  B8 = 8, // Left button in center cluster
-  B9 = 9, // Right button in center cluster
-  B10 = 10, // Left stick pressed button
-  B11 = 11, // Right stick pressed button
-  B12 = 12, // Top button in left cluster
-  B13 = 13, // Bottom button in left cluster
-  B14 = 14, // Left button in left cluster
-  B15 = 15, // Right button in left cluster
-  B16 = 16, // Center button in center cluster
-  B17 = 17, // 	Extra non-standard buttons
-}
-
-/**
- * Joystick axis
- */
-export enum Axis {
-  HORIZONTAL_LEFT = 0, // Horizontal axis for left stick (negative left/positive right)
-  VERTICAL_LEFT = 1, // Vertical axis for left stick (negative up/positive down)
-  HORIZONTAL_RIGHT = 2, // Horizontal axis for right stick (negative left/positive right)
-  VERTICAL_RIGHT = 3, // Vertical axis for right stick (negative up/positive down)
-}
-
-/**
- * Possible input
- */
-export interface InputSpec {
-  /**
-   * Input type (Axis or Button)
-   */
-  type: InputType.Axis | InputType.Button
-  /**
-   * Input value
-   */
-  value: Axis | Button
-}
-</script>
-
 <script setup lang="ts">
 import { v4 as uuid4 } from 'uuid'
 import { computed, onBeforeUnmount, ref, toRefs, watch } from 'vue'
 
 import type { InputWithPrettyName } from '@/libs/joystick/protocols'
 import { scale } from '@/libs/utils'
-import type { ProtocolControllerMapping } from '@/types/joystick'
+import { type JoystickInput, type ProtocolControllerMapping, JoystickAxis, JoystickButton } from '@/types/joystick'
 import { InputType } from '@/types/joystick'
 
 const textColor = '#747474'
@@ -71,32 +21,32 @@ enum Models {
   PS5 = 'PS5',
 }
 
-const buttonPath: { [key in Button]: string } = {
-  [Button.B0]: 'path_b0',
-  [Button.B1]: 'path_b1',
-  [Button.B2]: 'path_b2',
-  [Button.B3]: 'path_b3',
-  [Button.B4]: 'path_b4',
-  [Button.B5]: 'path_b5',
-  [Button.B6]: 'path_b6',
-  [Button.B7]: 'path_b7',
-  [Button.B8]: 'path_b8',
-  [Button.B9]: 'path_b9',
-  [Button.B10]: 'path_b10',
-  [Button.B11]: 'path_b11',
-  [Button.B12]: 'path_b12',
-  [Button.B13]: 'path_b13',
-  [Button.B14]: 'path_b14',
-  [Button.B15]: 'path_b15',
-  [Button.B16]: 'path_b16',
-  [Button.B17]: 'path_b17',
+const buttonPath: { [key in JoystickButton]: string } = {
+  [JoystickButton.B0]: 'path_b0',
+  [JoystickButton.B1]: 'path_b1',
+  [JoystickButton.B2]: 'path_b2',
+  [JoystickButton.B3]: 'path_b3',
+  [JoystickButton.B4]: 'path_b4',
+  [JoystickButton.B5]: 'path_b5',
+  [JoystickButton.B6]: 'path_b6',
+  [JoystickButton.B7]: 'path_b7',
+  [JoystickButton.B8]: 'path_b8',
+  [JoystickButton.B9]: 'path_b9',
+  [JoystickButton.B10]: 'path_b10',
+  [JoystickButton.B11]: 'path_b11',
+  [JoystickButton.B12]: 'path_b12',
+  [JoystickButton.B13]: 'path_b13',
+  [JoystickButton.B14]: 'path_b14',
+  [JoystickButton.B15]: 'path_b15',
+  [JoystickButton.B16]: 'path_b16',
+  [JoystickButton.B17]: 'path_b17',
 }
 
-const axisPath: { [key in Axis]: string } = {
-  [Axis.HORIZONTAL_LEFT]: 'path_b10',
-  [Axis.VERTICAL_LEFT]: 'path_b10',
-  [Axis.HORIZONTAL_RIGHT]: 'path_b11',
-  [Axis.VERTICAL_RIGHT]: 'path_b11',
+const axisPath: { [key in JoystickAxis]: string } = {
+  [JoystickAxis.HORIZONTAL_LEFT]: 'path_b10',
+  [JoystickAxis.VERTICAL_LEFT]: 'path_b10',
+  [JoystickAxis.HORIZONTAL_RIGHT]: 'path_b11',
+  [JoystickAxis.VERTICAL_RIGHT]: 'path_b11',
 }
 
 /* eslint-disable  */
@@ -133,13 +83,13 @@ const emit = defineEmits<{
   (e: 'click', inputs: any): void
 }>()
 
-const findInputFromPath = (path: string): InputSpec[] => {
-  const inputs: InputSpec[] = []
+const findInputFromPath = (path: string): JoystickInput[] => {
+  const inputs: JoystickInput[] = []
   Object.entries(buttonPath).filter(([, v]) => v === path).forEach((button) => {
-    inputs.push({ type: InputType.Button, value: button[0] as unknown as Button })
+    inputs.push({ type: InputType.Button, value: button[0] as unknown as JoystickButton })
   })
   Object.entries(axisPath).filter(([, v]) => v === path).forEach((axis) => {
-    inputs.push({ type: InputType.Axis, value: axis[0] as unknown as Axis })
+    inputs.push({ type: InputType.Axis, value: axis[0] as unknown as JoystickAxis })
   })
   return inputs
 }
@@ -188,8 +138,8 @@ onBeforeUnmount(async () => {
 watch(
   () => [props.leftAxisHoriz, props.leftAxisVert, props.rightAxisHoriz, props.rightAxisVert],
   () => {
-    setAxes([Axis.HORIZONTAL_LEFT, Axis.VERTICAL_LEFT], [props.leftAxisHoriz ?? 0, props.leftAxisVert ?? 0] )
-    setAxes([Axis.HORIZONTAL_RIGHT, Axis.VERTICAL_RIGHT], [props.rightAxisHoriz ?? 0, props.rightAxisVert ?? 0] )
+    setAxes([JoystickAxis.HORIZONTAL_LEFT, JoystickAxis.VERTICAL_LEFT], [props.leftAxisHoriz ?? 0, props.leftAxisVert ?? 0] )
+    setAxes([JoystickAxis.HORIZONTAL_RIGHT, JoystickAxis.VERTICAL_RIGHT], [props.rightAxisHoriz ?? 0, props.rightAxisVert ?? 0] )
   }
 )
 
@@ -226,9 +176,9 @@ const protocolMapping = toRefs(props).protocolMapping
 watch([protocolMapping, buttonLabelCorrespondency], () => updateLabelsState())
 
 const updateLabelsState = (): void => {
-  Object.values(Button).forEach((button) => {
+  Object.values(JoystickButton).forEach((button) => {
     if (isNaN(Number(button))) return
-    const protocolButton = props.protocolMapping.buttons[button as Button] || undefined
+    const protocolButton = props.protocolMapping.buttons[button as JoystickButton] || undefined
     let functionName = undefined
     if (props.buttonLabelCorrespondency.length === 0 || !props.buttonLabelCorrespondency.map((b) => b.input.value).includes(button)) {
       functionName = protocolButton.protocol && protocolButton.value ? `${protocolButton.value} (${protocolButton.protocol})` : 'unassigned'
@@ -248,34 +198,34 @@ const updateLabelsState = (): void => {
 }
 
 const updateButtonsState = (): void => {
-  toggleButton(Button.B0, props.b0 === undefined ? false : props.b0 > 0.5)
-  toggleButton(Button.B1, props.b1 === undefined ? false : props.b1 > 0.5)
-  toggleButton(Button.B2, props.b2 === undefined ? false : props.b2 > 0.5)
-  toggleButton(Button.B3, props.b3 === undefined ? false : props.b3 > 0.5)
-  toggleButton(Button.B4, props.b4 === undefined ? false : props.b4 > 0.5)
-  toggleButton(Button.B5, props.b5 === undefined ? false : props.b5 > 0.5)
-  toggleButton(Button.B6, props.b6 === undefined ? false : props.b6 > 0.5)
-  toggleButton(Button.B7, props.b7 === undefined ? false : props.b7 > 0.5)
-  toggleButton(Button.B8, props.b8 === undefined ? false : props.b8 > 0.5)
-  toggleButton(Button.B9, props.b9 === undefined ? false : props.b9 > 0.5)
-  toggleButton(Button.B10, props.b10 === undefined ? false : props.b10 > 0.5)
-  toggleButton(Button.B11, props.b11 === undefined ? false : props.b11 > 0.5)
-  toggleButton(Button.B12, props.b12 === undefined ? false : props.b12 > 0.5)
-  toggleButton(Button.B13, props.b13 === undefined ? false : props.b13 > 0.5)
-  toggleButton(Button.B14, props.b14 === undefined ? false : props.b14 > 0.5)
-  toggleButton(Button.B15, props.b15 === undefined ? false : props.b15 > 0.5)
-  toggleButton(Button.B16, props.b16 === undefined ? false : props.b16 > 0.5)
-  toggleButton(Button.B17, props.b17 === undefined ? false : props.b17 > 0.5)
+  toggleButton(JoystickButton.B0, props.b0 === undefined ? false : props.b0 > 0.5)
+  toggleButton(JoystickButton.B1, props.b1 === undefined ? false : props.b1 > 0.5)
+  toggleButton(JoystickButton.B2, props.b2 === undefined ? false : props.b2 > 0.5)
+  toggleButton(JoystickButton.B3, props.b3 === undefined ? false : props.b3 > 0.5)
+  toggleButton(JoystickButton.B4, props.b4 === undefined ? false : props.b4 > 0.5)
+  toggleButton(JoystickButton.B5, props.b5 === undefined ? false : props.b5 > 0.5)
+  toggleButton(JoystickButton.B6, props.b6 === undefined ? false : props.b6 > 0.5)
+  toggleButton(JoystickButton.B7, props.b7 === undefined ? false : props.b7 > 0.5)
+  toggleButton(JoystickButton.B8, props.b8 === undefined ? false : props.b8 > 0.5)
+  toggleButton(JoystickButton.B9, props.b9 === undefined ? false : props.b9 > 0.5)
+  toggleButton(JoystickButton.B10, props.b10 === undefined ? false : props.b10 > 0.5)
+  toggleButton(JoystickButton.B11, props.b11 === undefined ? false : props.b11 > 0.5)
+  toggleButton(JoystickButton.B12, props.b12 === undefined ? false : props.b12 > 0.5)
+  toggleButton(JoystickButton.B13, props.b13 === undefined ? false : props.b13 > 0.5)
+  toggleButton(JoystickButton.B14, props.b14 === undefined ? false : props.b14 > 0.5)
+  toggleButton(JoystickButton.B15, props.b15 === undefined ? false : props.b15 > 0.5)
+  toggleButton(JoystickButton.B16, props.b16 === undefined ? false : props.b16 > 0.5)
+  toggleButton(JoystickButton.B17, props.b17 === undefined ? false : props.b17 > 0.5)
 }
 
 /**
  * Change virtual button to on/off state
  *
- * @param {Button} button Button to be used
+ * @param {JoystickButton} button Button to be used
  * @param {boolean} state Button state
  * @returns {void}
  */
-function toggleButton(button: Button, state: boolean): void {
+function toggleButton(button: JoystickButton, state: boolean): void {
   svg?.getElementById(buttonPath[button])?.setAttribute('fill', state ? 'red' : 'transparent')
   svg
     ?.getElementById(buttonPath[button].replace('path', 'path_line'))
@@ -286,16 +236,16 @@ function toggleButton(button: Button, state: boolean): void {
 /**
  * Set axis position
  *
- * @param {[Axis.HORIZONTAL_LEFT, Axis.VERTICAL_LEFT] | [Axis.HORIZONTAL_RIGHT, Axis.VERTICAL_RIGHT]} axes Axes combination to be set
+ * @param {[JoystickAxis.HORIZONTAL_LEFT, JoystickAxis.VERTICAL_LEFT] | [JoystickAxis.HORIZONTAL_RIGHT, JoystickAxis.VERTICAL_RIGHT]} axes Axes combination to be set
  * @param {[number, number]} horizontalValue Horizontal and vertical axes values between [-1, 1]
  * @returns {void}
  */
-function setAxes(axes: [Axis.HORIZONTAL_LEFT, Axis.VERTICAL_LEFT] | [Axis.HORIZONTAL_RIGHT, Axis.VERTICAL_RIGHT], values: [number, number]): void {
+function setAxes(axes: [JoystickAxis.HORIZONTAL_LEFT, JoystickAxis.VERTICAL_LEFT] | [JoystickAxis.HORIZONTAL_RIGHT, JoystickAxis.VERTICAL_RIGHT], values: [number, number]): void {
   let xValue
   let yValue
   switch (props.model) {
     case Models.PS5: {
-      xValue = axes[0] == Axis.HORIZONTAL_LEFT ? scale(values[0], -1, 1, -3920.9, -3882.1) : scale(values[0], -1, 1, -4144.8, -4106.1)
+      xValue = axes[0] == JoystickAxis.HORIZONTAL_LEFT ? scale(values[0], -1, 1, -3920.9, -3882.1) : scale(values[0], -1, 1, -4144.8, -4106.1)
       yValue = scale(values[1], -1, 1, -2192.7, -2153.9)
       break
     }
