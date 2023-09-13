a3c2db8000128f6de1e901b84f5535f76a96f5d3
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-09-13 16:32:29 -0300

    Use monospace font on `DepthIndicator`
    This way the sensor value does not affect the widget size.

--- src/components/mini-widgets/DepthIndicator.vue
+++ src/components/mini-widgets/DepthIndicator.vue
@@ -2,7 +2,10 @@
   <div class="flex items-center w-fit min-w-[8rem] max-w-[9rem] h-12 p-1 text-white justify-center">
     <img src="@/assets/depth-icon.svg" class="h-full" :draggable="false" />
     <div class="flex flex-col items-start justify-center ml-1 min-w-[4rem] max-w-[6rem] select-none">
-      <span class="text-xl font-semibold leading-6 w-fit">{{ round(averageDepth, 2) }} m</span>
+      <div>
+        <span class="font-mono text-xl font-semibold leading-6 w-fit">{{ round(averageDepth, 2) }}</span>
+        <span class="text-xl font-semibold leading-6 w-fit"> m</span>
+      </div>
       <span class="w-full text-sm font-semibold leading-4 whitespace-nowrap">Depth</span>
     </div>
   </div>
