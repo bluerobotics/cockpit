0adec4ba66a4e9a359e9b0876a33e261e89e8493
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-11-17 16:47:58 -0300

    Persist allowd stream IPs between boots

--- src/stores/video.ts
+++ src/stores/video.ts
@@ -1,8 +1,10 @@
+import { useStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 
 export const useVideoStore = defineStore('video', () => {
   const availableIceIps = reactive<string[]>([])
+  const allowedIceIps = useStorage<string[]>('cockpit-allowed-stream-ips', [])
 
-  return { availableIceIps }
+  return { availableIceIps, allowedIceIps }
 })
