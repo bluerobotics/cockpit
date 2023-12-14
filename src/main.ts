add32fa91a5eafbd556bce53a23e9eef3128f067
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2023-12-14 12:38:18 -0300

    system-logging: Create library to intercept and save console logs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import 'floating-vue/dist/style.css'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+import '@/libs/system-logging'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { far } from '@fortawesome/free-regular-svg-icons'
