dc535e320b1f08878bf4f920d54a2acd3ecabb7f
wanderson <wandersoncoder@gmail.com>
2024-01-05 00:27:44 -0300

    config-alerts: Toggles should change colours when toggled

--- src/views/ConfigurationAlertsView.vue
+++ src/views/ConfigurationAlertsView.vue
@@ -2,11 +2,21 @@
   <BaseConfigurationView>
     <template #title>Alerts configuration</template>
     <template #content>
-      <v-switch v-model="alertStore.enableVoiceAlerts" label="Enable voice alerts" class="m-2 text-slate-800" />
+      <v-switch
+        v-model="alertStore.enableVoiceAlerts"
+        label="Enable voice alerts"
+        class="m-2 text-slate-800"
+        color="rgb(0, 20, 80)"
+      />
       <span class="text-sm font-medium text-slate-500">Enable voice on specific alert levels:</span>
       <div class="flex items-center justify-start">
         <div v-for="enabledLevel in alertStore.enabledAlertLevels" :key="enabledLevel.level" class="mx-2">
-          <v-switch v-model="enabledLevel.enabled" :label="capitalize(enabledLevel.level)" class="text-slate-800" />
+          <v-switch
+            v-model="enabledLevel.enabled"
+            :label="capitalize(enabledLevel.level)"
+            class="text-slate-800"
+            color="rgb(0, 20, 80)"
+          />
         </div>
       </div>
       <span class="text-sm font-medium text-slate-500">Alert voice:</span>
