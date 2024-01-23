b382951f2d54cda0ca7fa362fa0a1e54b5274ed8
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-01-23 17:38:24 -0300

    vehicle-settings: Store vehicle settings inside it (instead of local browser storage)

--- src/stores/development.ts
+++ src/stores/development.ts
@@ -1,12 +1,13 @@
-import { useStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+
+import { useBlueOsStorage } from '@/composables/settingsSyncer'
 
 export const systemLoggingEnablingKey = 'cockpit-enable-system-logging'
 export const useDevelopmentStore = defineStore('development', () => {
   const developmentMode = ref(false)
   const widgetDevInfoBlurLevel = ref(3)
-  const enableSystemLogging = useStorage(systemLoggingEnablingKey, true)
+  const enableSystemLogging = useBlueOsStorage(systemLoggingEnablingKey, true)
 
   return { developmentMode, widgetDevInfoBlurLevel, enableSystemLogging }
 })
