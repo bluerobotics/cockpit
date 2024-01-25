7eaf52c7d52b2940f9c9fff67131fd20fad08cd7
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-01-25 20:15:49 -0300

    views: Allow toggling views visibility

--- src/components/mini-widgets/ViewSelector.vue
+++ src/components/mini-widgets/ViewSelector.vue
@@ -3,7 +3,7 @@
     <Dropdown
       name-key="name"
       :model-value="widgetStore.currentView"
-      :options="widgetStore.currentProfile.views"
+      :options="widgetStore.viewsToShow"
       class="min-w-[128px]"
       @update:model-value="widgetStore.selectView"
     />
