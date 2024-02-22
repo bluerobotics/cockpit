2882f635960770ff150c361badbb849f17fc2dfe
Eric <eric.emj.johnson@gmail.com>
2024-02-21 20:00:54 -0800

    takeoff: fixing bug with flying variable being inverse

--- src/components/mini-widgets/TakeoffLandCommander.vue
+++ src/components/mini-widgets/TakeoffLandCommander.vue
@@ -1,10 +1,10 @@
 <template>
   <button
     class="relative flex items-center justify-center w-32 p-1 rounded-md shadow-inner h-9 bg-slate-800/60 hover:bg-slate-400/60"
-    @click="vehicleStore.flying ? vehicleStore.takeoff() : vehicleStore.land()"
+    @click="vehicleStore.flying ? vehicleStore.land() : vehicleStore.takeoff()"
   >
     <span class="inline-block font-extrabold align-middle text-white">
-      {{ vehicleStore.flying === undefined ? '...' : vehicleStore.flying ? 'Takeoff' : 'Land' }}
+      {{ vehicleStore.flying === undefined ? '...' : vehicleStore.flying ? 'Land' : 'Takeoff' }}
     </span>
   </button>
 </template>
