4201bdd29971e11f1f1a5443ac6215f2559caffd
Eric <eric.emj.johnson@gmail.com>
2024-02-21 20:01:53 -0800

    widget: added widget to change altitude

---
+++ src/components/mini-widgets/ChangeAltitudeCommander.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+    :class="[
+      'relative flex items-center justify-center w-32 p-1 rounded-md shadow-inner h-9',
+      vehicleStore.flying ? 'bg-slate-800/60 hover:bg-slate-400/60' : 'bg-slate-400/60 cursor-not-allowed',
+    ]"
+    :disabled="!vehicleStore.flying"
+    @click="vehicleStore.changeAlt()"
+  >
+    <span class="inline-block font-extrabold align-middle text-white"> Change Alt </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useMainVehicleStore } from '@/stores/mainVehicle'
+
+const vehicleStore = useMainVehicleStore()
+</script>
