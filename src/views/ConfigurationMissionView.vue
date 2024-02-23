44da938d4586e7c20146d35a9c122d079d90e845
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-02-23 09:37:25 -0300

    views: Add mission configuration view
     * Add a new Mission configuration view in the configurations menu
     to allow users to enable/disable slide to confirm events

---
+++ src/views/ConfigurationMissionView.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseConfigurationView>
+    <template #title>Mission configuration</template>
+    <template #content>
+      <v-switch
+        v-model="missionStore.slideEventsEnabled"
+        label="Enable slide to confirm"
+        class="m-2 text-slate-800"
+        color="rgb(0, 20, 80)"
+      />
+      <div class="flex w-[60%] flex-wrap">
+        <v-checkbox
+          v-for="variable in EventCategory"
+          :key="variable"
+          v-model="missionStore.slideEventsCategoriesRequired[variable]"
+          :disabled="!missionStore.slideEventsEnabled"
+          :label="variable"
+        ></v-checkbox>
+      </div>
+    </template>
+  </BaseConfigurationView>
+</template>
+
+<script setup lang="ts">
+import { EventCategory } from '@/libs/slide-to-confirm'
+import { useMissionStore } from '@/stores/mission'
+
+import BaseConfigurationView from './BaseConfigurationView.vue'
+
+const missionStore = useMissionStore()
+</script>
