83bb29d505c577945c488dc666add58e6e684b01
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-02-23 08:56:10 -0300

    libs: slide-to-confirm: Add event categories
     * Add events categories on all requests for slide to confirm actions

--- src/libs/slide-to-confirm.ts
+++ src/libs/slide-to-confirm.ts
@@ -6,13 +6,46 @@ export const confirmationSliderText = ref('Action Confirm')
 export const confirmed = ref(false)
 
 /**
+ * Different categories of events that requires confirmation from the user.
+ * @enum {string}
+ */
+export enum EventCategory {
+  ARM = 'arm',
+  DISARM = 'disarm',
+  TAKEOFF = 'takeoff',
+  ALT_CHANGE = 'alt_change',
+  LAND = 'land',
+  GOTO = 'goto',
+}
+
+/**
+ * Specify if by default event categories need to be confirmed by the user.
+ * @type {boolean}
+ */
+const defaultCategoriesRequired = true
+
+/**
+ * The default mapping of event categories to the confirmation requirement.
+ * @type {Record<string, boolean>}
+ */
+export const eventCategoriesDefaultMapping: Record<string, boolean> = Object.values(EventCategory).reduce(
+  (acc: Record<string, boolean>, v) => {
+    acc[v] = defaultCategoriesRequired
+    return acc
+  },
+  {}
+)
+
+/**
  * Waits for user confirmation through the slide-to-confirm component.
+ * @param {EventCategory} category - The category of the event that requires confirmation.
  * @param {string} text - The custom text to display on the slider.
  * @param {string} confirmationText - The custom text to display on the confirmation slider after the user slides to confirm.
  * @returns {Promise<boolean>} - A promise that resolves with true when the action is confirmed and false when the user cancels the action.
  */
-export function slideToConfirm(text: string, confirmationText: string): Promise<boolean> {
-  console.log('slideToConfirm with text:', text)
+export function slideToConfirm(category: EventCategory, text: string, confirmationText: string): Promise<boolean> {
+  console.log(`slideToConfirm from category ${category} with text: ${text}`)
+
   return new Promise((resolve) => {
     sliderText.value = text
     confirmationSliderText.value = confirmationText
