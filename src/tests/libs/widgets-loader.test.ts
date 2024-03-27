3a0680a1fa1b1f5294b5859660550bea476ccdaf
Patrick José Pereira <patrickelectric@gmail.com>
2024-03-27 09:30:52 -0300

    tests: widgets-loader: Update to add mini-widgets test
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/tests/libs/widgets-loader.test.ts
+++ src/tests/libs/widgets-loader.test.ts
@@ -1,10 +1,25 @@
 import { expect, test } from 'vitest'
 
+import { MiniWidgetType } from '@/types/miniWidgets'
 import { WidgetType } from '@/types/widgets'
 
 test('Test widgets exist', async () => {
+  await enum_to_files_checker(WidgetType, '@/components/widgets/${name}.vue')
+})
+
+test('Test mini-widgets exist', async () => {
+  await enum_to_files_checker(MiniWidgetType, '@/components/mini-widgets/${name}.vue')
+})
+
+/**
+ * Test helper to verify if all enums names matches with a file list in a path
+ * @param {T} enum_type Enum type with values as filenames
+ * @param {string} path_template Path template as string, where name should be used as filename
+ */
+async function enum_to_files_checker<T>(enum_type: T, path_template: string): Promise<void> {
   const loader = await Promise.allSettled(
-    Object.values(WidgetType).map((name) => import(`@/components/widgets/${name}.vue`))
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    Object.values(enum_type).map((name) => import(eval('`' + path_template + '`'))) // Please, have marcy of my soul
   )
   const found_all_files = loader.every((file) => {
     if (file.status == 'fulfilled') {
@@ -17,4 +32,4 @@ test('Test widgets exist', async () => {
     return !failed_to_find
   })
   expect(found_all_files).toBe(true)
-})
+}
