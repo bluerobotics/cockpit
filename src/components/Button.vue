cabb7dfdebff444e08e6d05062dbe04e838ad2d7
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-04-09 21:37:03 -0300

    button: Bind attributes passed on instantiation

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     class="px-4 py-1 text-lg font-medium text-white uppercase transition-all rounded-md shadow-md action-button bg-slate-500 hover:bg-slate-400"
+    v-bind="$attrs"
   >
     <slot></slot>
   </button>
