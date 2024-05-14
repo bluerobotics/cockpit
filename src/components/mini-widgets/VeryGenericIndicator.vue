33566ad806f89e1b0ea7f372c69d6f8cc0974f45
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-05-14 14:07:50 -0300

    generic-variables: Use slashes instead of dots for variable path defyning

--- src/components/mini-widgets/VeryGenericIndicator.vue
+++ src/components/mini-widgets/VeryGenericIndicator.vue
@@ -16,7 +16,7 @@
     persistent
     class="w-[100vw] flex justify-center items-center"
   >
-    <v-card class="configModal p-8">
+    <v-card class="p-8 configModal">
       <div class="close-icon mdi mdi-close" @click.stop="closeDialog"></div>
       <v-card-title class="text-white">
         <div class="flex items-center mb-3 mt-[-5px] justify-evenly">
@@ -141,7 +141,7 @@
         <div
           v-for="(template, i) in veryGenericIndicatorPresets"
           :key="i"
-          class="flex items-center m-2 rounded-md px-2 text-white justify-center cursor-pointer hover:bg-slate-100/20 transition-all"
+          class="flex items-center justify-center px-2 m-2 text-white transition-all rounded-md cursor-pointer hover:bg-slate-100/20"
           @click="setIndicatorFromTemplate(template)"
         >
           <span class="relative w-[2rem] mdi icon-symbol text-[34px] mx-2" :class="[template.iconName]"></span>
@@ -284,6 +284,12 @@ watch(
   { deep: true }
 )
 onMounted(() => {
+  // Update old variables naming to new pattern
+  // TODO: Remove this before 1.0.0 release
+  if (miniWidget.value.options.variableName) {
+    miniWidget.value.options.variableName = miniWidget.value.options.variableName.replaceAll('.', '/')
+  }
+
   updateVariableState()
   updateWidgetName()
   updateGenericVariablesNames()
