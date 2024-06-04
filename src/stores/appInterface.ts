8619b0f0641064955b2b77d181b1dfd53093e176
Arturo Manzoli <arturomanzoli@gmail.com>
2024-06-04 13:46:53 -0300

    Store: Interface: Add responsivity logic and interface control store
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

---
+++ src/stores/appInterface.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+
+export const useAppInterfaceStore = defineStore('responsive', {
+  state: () => ({
+    width: window.innerWidth,
+  }),
+  actions: {
+    updateWidth() {
+      this.width = window.innerWidth
+    },
+  },
+  getters: {
+    isXs: (state) => state.width < 720, // Extra small devices (5-6" mobile screens in landscape)
+    isSm: (state) => state.width >= 720 && state.width < 980, // Small devices (6-7" mobile screens in landscape)
+    isMd: (state) => state.width >= 980 && state.width < 1280, // Medium devices (7-10" tablets in landscape)
+    isLg: (state) => state.width >= 1280 && state.width < 1600, // Large devices (HD in landscape)
+    isXl: (state) => state.width >= 1600 && state.width < 1920, // Extra large devices (HD+ desktop/laptop screens in landscape)
+    is2xl: (state) => state.width >= 1920, // FullHD and above - Extra extra large devices
+    currentWindowBreakpoint: (state) => {
+      if (state.width < 720) return 'xs'
+      if (state.width >= 720 && state.width < 980) return 'sm'
+      if (state.width >= 980 && state.width < 1280) return 'md'
+      if (state.width >= 1280 && state.width < 1600) return 'lg'
+      if (state.width >= 1600 && state.width < 1920) return 'xl'
+      if (state.width >= 1920) return '2xl'
+    },
+    currentWindowSize: (state) => `${state.width} x ${window.innerHeight}`,
+  },
+})
