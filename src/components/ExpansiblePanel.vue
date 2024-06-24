cea4aa4b270cdf7a64d85da2a2cd50efdf5b172c
Arturo Manzoli <arturomanzoli@gmail.com>
2024-06-24 15:13:17 -0300

    Components: Expansible-panel: Add compact mode and hover, and decorations
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

--- src/components/ExpansiblePanel.vue
+++ src/components/ExpansiblePanel.vue
@@ -1,23 +1,44 @@
 <template>
-  <div class="bg-transparent text-white w-full">
+  <div class="text-white w-full">
     <div class="flex flex-col align-start">
       <v-divider v-if="!noTopDivider" centered class="opacity-10 border-[#fafafa] w-full" />
-      <div class="flex flex-row w-full justify-between">
-        <div class="flex flex-row align-center gap-x-[3vw] pb-[1.5vh] pt-[1.2vh] cursor-pointer" @click="togglePanel">
+      <div
+        :class="[
+          isPanelExpanded && isMarkExpanded ? 'panel-header-expanded elevation-1' : 'panel-header elevation-0',
+          isHoverEffect ? 'hover:bg-[#ffffff11]' : '',
+        ]"
+        @click="togglePanel"
+      >
+        <div
+          class="flex flex-row align-center"
+          :class="isCompact ? 'gap-x-[1vw] py-[0.8vh]' : 'gap-x-[3vw] py-[1.5vh]'"
+        >
           <div>
             <div class="flex flex-row">
               <v-icon
-                :size="24"
+                :size="isCompact ? 20 : 24"
                 :icon="isPanelExpanded ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-                class="mr-2 mt-[1px]"
+                :class="
+                  interfaceStore.isOnSmallScreen
+                    ? isCompact
+                      ? '-mt-[2px] mr-[3px]'
+                      : '-mt-[3px] mr-[3px]'
+                    : 'mt-[2px] mr-[2px]'
+                "
               />
               <div class="flex flex-col">
-                <div class="font-semibold" :class="interfaceStore.isOnSmallScreen ? 'text-[14px]' : 'text-[18px]'">
+                <div
+                  class="font-semibold"
+                  :class="interfaceStore.isOnSmallScreen ? `text-[${textSize - 4}px]` : `text-[${textSize}px]`"
+                >
                   <slot name="title" />
                 </div>
               </div>
             </div>
-            <div class="font-light opacity-50" :class="interfaceStore.isOnSmallScreen ? 'text-[10px]' : 'text-[14px]'">
+            <div
+              class="font-normal"
+              :class="interfaceStore.isOnSmallScreen ? `text-[${textSize - 8}px]` : `text-[${textSize - 4}px]`"
+            >
               <slot name="subtitle" />
             </div>
           </div>
@@ -29,29 +50,37 @@
             size="small"
             color="transparent"
             elevation="0"
-            @click="toggleAlert"
+            @click="toggleInfo"
           />
         </div>
       </div>
       <v-divider v-if="!isPanelExpanded && !noBottomDivider" class="opacity-10 border-[#fafafa] w-full" />
     </div>
-    <div class="alert-container">
+    <div class="info-container">
       <div
-        ref="alertContent"
+        ref="infoContent"
         :class="[
-          'alert-expand-collapse',
-          { 'alert-expanding': isBottomSheetOpen, 'alert-collapsing': !isBottomSheetOpen },
+          'info-content-expand-collapse',
+          { 'info-expanding': isBottomSheetOpen, 'info-collapsing': !isBottomSheetOpen },
         ]"
       >
         <div
-          class="bg-[#ffffff11] py-3 px-5 rounded-[6px] text-white elevation-1 mb-2"
+          class="bg-[#ffffff11] py-3 px-5 rounded-[6px] text-white elevation-1 mb-2 mt-1"
           :class="interfaceStore.isOnSmallScreen ? 'text-[12px]' : 'text-[14px]'"
         >
           <slot name="info"></slot>
         </div>
       </div>
     </div>
-    <div ref="content" :class="['expand-collapse', { expanding: isPanelExpanded, collapsing: !isPanelExpanded }]">
+    <div
+      ref="content"
+      :class="[
+        'content-expand-collapse',
+        { expanding: isPanelExpanded, collapsing: !isPanelExpanded },
+        isDarkenContent ? 'bg-[#00000015]' : 'bg-transparent',
+        interfaceStore.isOnPhoneScreen ? 'px-1' : 'px-2',
+      ]"
+    >
       <slot name="content"></slot>
       <v-divider v-if="!noBottomDivider" centered class="opacity-10 border-[#fafafa] w-full" />
     </div>
@@ -78,6 +107,22 @@ const props = defineProps<{
    *
    */
   isExpanded?: boolean
+  /**
+   * Mark the panel header as expanded.
+   */
+  markExpanded?: boolean
+  /**
+   * Compact mode.
+   */
+  compact?: boolean
+  /**
+   * Darken the content background.
+   */
+  darkenContent?: boolean
+  /**
+   * Disable hover effect on panel header.
+   */
+  hoverEffect?: boolean
 }>()
 
 const slots = useSlots()
@@ -86,15 +131,26 @@ const isPanelExpanded = ref(props.isExpanded ?? false)
 const noTopDivider = ref(props.noTopDivider ?? false)
 const noBottomDivider = ref(props.noBottomDivider ?? false)
 const isBottomSheetOpen = ref(false)
+const isMarkExpanded = ref(props.markExpanded ?? false)
+const isCompact = ref(props.compact ?? false)
+const isDarkenContent = ref(props.darkenContent ?? false)
+const isHoverEffect = ref(props.hoverEffect ?? false)
 
 const content = ref<HTMLElement | null>(null)
-const alertContent = ref<HTMLElement | null>(null)
+const infoContent = ref<HTMLElement | null>(null)
 
 const togglePanel = (): void => {
   isPanelExpanded.value = !isPanelExpanded.value
 }
 
-const toggleAlert = (): void => {
+const textSize = computed(() => {
+  if (isCompact.value) {
+    return 16
+  }
+  return 18
+})
+
+const toggleInfo = (): void => {
   isBottomSheetOpen.value = !isBottomSheetOpen.value
 }
 
@@ -112,13 +168,13 @@ watch(isPanelExpanded, (newValue) => {
 })
 
 watch(isBottomSheetOpen, (newValue) => {
-  if (alertContent.value) {
+  if (infoContent.value) {
     if (newValue) {
-      alertContent.value.style.maxHeight = alertContent.value.scrollHeight + 'px'
+      infoContent.value.style.maxHeight = infoContent.value.scrollHeight + 'px'
     } else {
-      alertContent.value.style.maxHeight = alertContent.value.scrollHeight + 'px'
+      infoContent.value.style.maxHeight = infoContent.value.scrollHeight + 'px'
       setTimeout(() => {
-        alertContent.value!.style.maxHeight = '0px'
+        infoContent.value!.style.maxHeight = '0px'
       }, 0)
     }
   }
@@ -128,8 +184,8 @@ onMounted(() => {
   if (content.value && !isPanelExpanded.value) {
     content.value.style.maxHeight = '0px'
   }
-  if (alertContent.value && !isBottomSheetOpen.value) {
-    alertContent.value.style.maxHeight = '0px'
+  if (infoContent.value && !isBottomSheetOpen.value) {
+    infoContent.value.style.maxHeight = '0px'
   }
 })
 
@@ -137,16 +193,34 @@ const hasInfoSlot = computed(() => !!slots.info?.())
 </script>
 
 <style scoped>
-.expand-collapse {
+.panel-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  background-color: transparent;
+  width: 100%;
+  padding-left: 5px;
+  cursor: pointer;
+}
+.panel-header-expanded {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  background-color: rgba(255, 255, 255, 0.1);
+  width: 100%;
+  padding-left: 5px;
+  cursor: pointer;
+}
+.content-expand-collapse {
   overflow: hidden;
   transition: max-height 0.3s ease;
 }
 
-.alert-container {
+.info-container {
   overflow: hidden;
 }
 
-.alert-expand-collapse {
+.info-content-expand-collapse {
   overflow: hidden;
   transition: max-height 0.3s ease;
 }
