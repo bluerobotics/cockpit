25591d679f01044aa0c48a070d9c30f5673319ce
Arturo Manzoli <arturomanzoli@gmail.com>
2024-06-25 13:38:15 -0300

    Components: Button: Adapt component to new UI
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-4 py-1 text-lg font-medium text-white uppercase transition-all rounded-md shadow-md action-button bg-slate-500 hover:bg-slate-400"
+    class="px-4 py-1 text-lg font-medium text-white uppercase transition-all rounded-md shadow-md action-button bg-[#00000055] hover:bg-[#00000033]"
     v-bind="$attrs"
   >
     <slot></slot>
