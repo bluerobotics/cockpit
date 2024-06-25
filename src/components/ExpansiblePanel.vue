c89917bcbc9fcc4aff105261459e842cf1fbd97a
Arturo Manzoli <arturomanzoli@gmail.com>
2024-06-25 13:39:21 -0300

    Components: Expansible-panel: Add warning messages display
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

--- src/components/ExpansiblePanel.vue
+++ src/components/ExpansiblePanel.vue
@@ -14,7 +14,7 @@
           :class="isCompact ? 'gap-x-[1vw] py-[0.8vh]' : 'gap-x-[3vw] py-[1.5vh]'"
         >
           <div>
-            <div class="flex flex-row">
+            <div class="flex flex-row whitespace-nowrap">
               <v-icon
                 :size="isCompact ? 20 : 24"
                 :icon="isPanelExpanded ? 'mdi-chevron-up' : 'mdi-chevron-down'"
@@ -43,15 +43,24 @@
             </div>
           </div>
         </div>
-        <div v-if="hasInfoSlot" class="flex items-center w-[10%]">
-          <v-btn
-            icon="mdi-information-outline"
-            class="ml-auto"
-            size="small"
-            color="transparent"
-            elevation="0"
-            @click="toggleInfo"
-          />
+        <div class="flex w-full justify-between">
+          <div v-if="hasInfoSlot" class="flex items-center w-[10%]">
+            <v-btn class="ml-auto rounded-full" size="small" color="transparent" elevation="0" @click.stop="toggleInfo">
+              <v-icon size="20" color="white" icon="mdi-information-outline" />
+            </v-btn>
+          </div>
+          <div v-if="hasWarningSlot" class="flex justify-end items-center relative">
+            <v-btn
+              class="ml-auto w-[10px] rounded-full"
+              size="small"
+              color="transparent"
+              elevation="0"
+              @click.stop="toggleWarning"
+            >
+              <v-icon size="20" color="yellow-400" icon="mdi-alert" />
+              <div v-if="animateWarning" class="ripple"></div>
+            </v-btn>
+          </div>
         </div>
       </div>
       <v-divider v-if="!isPanelExpanded && !noBottomDivider" class="opacity-10 border-[#fafafa] w-full" />
@@ -59,16 +68,29 @@
     <div class="info-container">
       <div
         ref="infoContent"
-        :class="[
-          'info-content-expand-collapse',
-          { 'info-expanding': isBottomSheetOpen, 'info-collapsing': !isBottomSheetOpen },
-        ]"
+        :class="['info-content-expand-collapse', { 'info-expanding': isInfoOpen, 'info-collapsing': !isInfoOpen }]"
       >
         <div
-          class="bg-[#ffffff11] py-3 px-5 rounded-[6px] text-white elevation-1 mb-2 mt-1"
+          class="bg-[#00000033] py-3 px-5 rounded-[6px] text-white elevation-1 mb-2 mt-1"
           :class="interfaceStore.isOnSmallScreen ? 'text-[12px]' : 'text-[14px]'"
         >
           <slot name="info"></slot>
+        </div>
+      </div>
+    </div>
+    <div class="warning-container">
+      <div
+        ref="warningContent"
+        :class="[
+          'info-content-expand-collapse',
+          { 'info-expanding': isWarningOpen, 'info-collapsing': !isWarningOpen },
+        ]"
+      >
+        <div
+          class="bg-[#00000033] py-3 px-5 rounded-[6px] text-white elevation-1 mb-2 mt-1"
+          :class="interfaceStore.isOnSmallScreen ? 'text-[12px]' : 'text-[14px]'"
+        >
+          <slot name="warning"></slot>
         </div>
       </div>
     </div>
@@ -120,7 +142,7 @@ const props = defineProps<{
    */
   darkenContent?: boolean
   /**
-   * Disable hover effect on panel header.
+   * Disable hover effect on the panel header.
    */
   hoverEffect?: boolean
 }>()
@@ -130,7 +152,8 @@ const slots = useSlots()
 const isPanelExpanded = ref(props.isExpanded ?? false)
 const noTopDivider = ref(props.noTopDivider ?? false)
 const noBottomDivider = ref(props.noBottomDivider ?? false)
-const isBottomSheetOpen = ref(false)
+const isInfoOpen = ref(false)
+const isWarningOpen = ref(false)
 const isMarkExpanded = ref(props.markExpanded ?? false)
 const isCompact = ref(props.compact ?? false)
 const isDarkenContent = ref(props.darkenContent ?? false)
@@ -138,20 +161,24 @@ const isHoverEffect = ref(props.hoverEffect ?? false)
 
 const content = ref<HTMLElement | null>(null)
 const infoContent = ref<HTMLElement | null>(null)
+const warningContent = ref<HTMLElement | null>(null)
+const animateWarning = ref(true)
 
 const togglePanel = (): void => {
   isPanelExpanded.value = !isPanelExpanded.value
 }
 
 const textSize = computed(() => {
-  if (isCompact.value) {
-    return 16
-  }
-  return 18
+  return isCompact.value ? 16 : 18
 })
 
 const toggleInfo = (): void => {
-  isBottomSheetOpen.value = !isBottomSheetOpen.value
+  isInfoOpen.value = !isInfoOpen.value
+}
+
+const toggleWarning = (): void => {
+  animateWarning.value = false
+  isWarningOpen.value = !isWarningOpen.value
 }
 
 watch(isPanelExpanded, (newValue) => {
@@ -167,7 +194,7 @@ watch(isPanelExpanded, (newValue) => {
   }
 })
 
-watch(isBottomSheetOpen, (newValue) => {
+watch(isInfoOpen, (newValue) => {
   if (infoContent.value) {
     if (newValue) {
       infoContent.value.style.maxHeight = infoContent.value.scrollHeight + 'px'
@@ -180,16 +207,33 @@ watch(isBottomSheetOpen, (newValue) => {
   }
 })
 
+watch(isWarningOpen, (newValue) => {
+  if (warningContent.value) {
+    if (newValue) {
+      warningContent.value.style.maxHeight = warningContent.value.scrollHeight + 'px'
+    } else {
+      warningContent.value.style.maxHeight = warningContent.value.scrollHeight + 'px'
+      setTimeout(() => {
+        warningContent.value!.style.maxHeight = '0px'
+      }, 0)
+    }
+  }
+})
+
 onMounted(() => {
   if (content.value && !isPanelExpanded.value) {
     content.value.style.maxHeight = '0px'
   }
-  if (infoContent.value && !isBottomSheetOpen.value) {
+  if (infoContent.value && !isInfoOpen.value) {
     infoContent.value.style.maxHeight = '0px'
+  }
+  if (warningContent.value && !isWarningOpen.value) {
+    warningContent.value.style.maxHeight = '0px'
   }
 })
 
 const hasInfoSlot = computed(() => !!slots.info?.())
+const hasWarningSlot = computed(() => !!slots.warning?.())
 </script>
 
 <style scoped>
@@ -223,5 +267,28 @@ const hasInfoSlot = computed(() => !!slots.info?.())
 .info-content-expand-collapse {
   overflow: hidden;
   transition: max-height 0.3s ease;
+}
+
+.ripple {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100px;
+  height: 100px;
+  background: rgba(255, 255, 0, 0.2);
+  border-radius: 50%;
+  transform: translate(-50%, -50%) scale(0);
+  animation: rippleEffect 1.5s infinite;
+}
+
+@keyframes rippleEffect {
+  0% {
+    transform: translate(-50%, -50%) scale(0);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(-50%, -50%) scale(0.6);
+    opacity: 0;
+  }
 }
 </style>
