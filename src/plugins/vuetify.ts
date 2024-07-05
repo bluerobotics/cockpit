bb8de45a5146b86e4fe07248eba777314ca5c4b9
Arturo Manzoli <arturomanzoli@gmail.com>
2024-07-05 08:28:23 -0300

    Libs: Rollback vuetify to v3.5.8
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,10 +3,5 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 import { createVuetify } from 'vuetify'
-import { VNumberInput } from 'vuetify/labs/VNumberInput'
 
-export default createVuetify({
-  components: {
-    VNumberInput,
-  },
-})
+export default createVuetify()
