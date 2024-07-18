8f0e1ec55990a2ac9796b5d93a6394a7a6a9e140
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-07-18 19:19:47 -0300

    depth-indicator: Improve it so the numbers and icon don't move

--- src/components/mini-widgets/DepthIndicator.vue
+++ src/components/mini-widgets/DepthIndicator.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex items-center w-[8.25rem] h-12 p-1 text-white justify-center">
-    <img src="@/assets/depth-icon.svg" class="h-full" :draggable="false" />
-    <div class="flex flex-col items-start justify-center ml-1 min-w-[4rem] max-w-[6rem] select-none">
+  <div class="w-[8.5rem] h-12 p-1 text-white relative">
+    <img src="@/assets/depth-icon.svg" class="h-[80%] left-[0.5rem] top-[13%] absolute" :draggable="false" />
+    <div class="absolute left-[3rem] flex flex-col items-start justify-center select-none">
       <div>
-        <span class="font-mono text-xl font-semibold leading-6 w-fit">{{ finalDepth.toPrecision(precision) }}</span>
+        <span class="font-mono text-xl font-semibold leading-6 w-fit">{{ finalDepth.toFixed(precision) }}</span>
         <span class="text-xl font-semibold leading-6 w-fit"> m</span>
       </div>
       <span class="w-full text-sm font-semibold leading-4 whitespace-nowrap">Depth</span>
@@ -25,10 +25,8 @@ watch(store.altitude, () => (depth.value = -store.altitude.msl))
 const finalDepth = computed(() => (depth.value < 0.01 ? 0 : depth.value))
 const precision = computed(() => {
   const fDepth = finalDepth.value
-  if (fDepth < 0.1) return 1
-  if (fDepth < 1) return 2
-  if (fDepth >= 1 && fDepth < 100) return 3
-  if (fDepth >= 10000) return 5
-  return 4
+  if (fDepth < 10) return 2
+  if (fDepth >= 10 && fDepth < 1000) return 1
+  return 0
 })
 </script>
