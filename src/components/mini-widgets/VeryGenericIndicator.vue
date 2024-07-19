b5809438fbaec68b46a4a9778028f7762041140b
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-07-18 21:43:24 -0300

    vgi: Improve number of decimal places for positive values

--- src/components/mini-widgets/VeryGenericIndicator.vue
+++ src/components/mini-widgets/VeryGenericIndicator.vue
@@ -215,9 +215,13 @@ const parsedState = computed(() => {
   if (value <= -10000 && value > -100000) return `${(value / 1000).toFixed(0)}k`
   if (value <= -100000) return '-∞'
 
-  if (value < 1) return value.toFixed(2)
-  if (value >= 1 && value < 100) return value.toFixed(1)
-  if (value >= 10000) return `${(value / 10000).toFixed(0)}k`
+  if (value < 1) return value.toFixed(3)
+  if (value >= 1 && value < 100) return value.toFixed(2)
+  if (value >= 100 && value < 1000) return value.toFixed(1)
+  if (value >= 1000 && value < 10000) return value.toFixed(0)
+  if (value >= 10000 && value < 100000) return `${(value / 1000).toFixed(1)}k`
+  if (value >= 100000 && value < 1000000) return `${(value / 1000).toFixed(0)}k`
+  if (value >= 1000000) return '+∞'
   return value.toFixed(0)
 })
 
