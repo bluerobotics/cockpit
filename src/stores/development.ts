13ce42f483af20e13d3fc6b05dc16c027ac00906
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-07-30 16:06:11 -0300

    telemetry: Allow user to opt out of sending usage statistics

--- src/stores/development.ts
+++ src/stores/development.ts
@@ -6,12 +6,20 @@ import { useBlueOsStorage } from '@/composables/settingsSyncer'
 
 export const systemLoggingEnablingKey = 'cockpit-enable-system-logging'
 export const blueOsSettingsSyncEnablingKey = 'cockpit-enable-blueos-settings-sync'
+export const enableUsageStatisticsTelemetryKey = 'cockpit-enable-usage-statistics-telemetry'
 
 export const useDevelopmentStore = defineStore('development', () => {
   const developmentMode = ref(false)
   const widgetDevInfoBlurLevel = ref(3)
   const enableSystemLogging = useBlueOsStorage(systemLoggingEnablingKey, true)
   const enableBlueOsSettingsSync = useStorage(blueOsSettingsSyncEnablingKey, true)
+  const enableUsageStatisticsTelemetry = useStorage(enableUsageStatisticsTelemetryKey, true)
 
-  return { developmentMode, widgetDevInfoBlurLevel, enableSystemLogging, enableBlueOsSettingsSync }
+  return {
+    developmentMode,
+    widgetDevInfoBlurLevel,
+    enableSystemLogging,
+    enableBlueOsSettingsSync,
+    enableUsageStatisticsTelemetry,
+  }
 })
