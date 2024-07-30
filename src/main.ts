2d6ce6cfb0b6b71e572e5a6a26256c79e2f1c281
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2024-07-30 15:59:59 -0300

    telemetry: Initialize basic telemetry with `sentry`

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import * as Sentry from '@sentry/vue'
 import FloatingVue from 'floating-vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
@@ -20,6 +21,18 @@ library.add(fas, far)
 loadFonts()
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: 'https://24f33bd0a8e35e7505da20846dcbca92@o4507696465707008.ingest.us.sentry.io/4507696619061248',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 0.1, //  Capture 10% of the transactions
+  tracePropagationTargets: [],
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  transport: Sentry.makeBrowserOfflineTransport(Sentry.makeFetchTransport),
+})
+
 app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.use(router).use(vuetify).use(createPinia()).use(FloatingVue).use(VueVirtualScroller)
 app.mount('#app')
