45691ae00da266b4b2c27a5c076a1b1a41cfff90
ArturoManzoli <arturomanzoli@gmail.com>
2024-12-09 16:39:52 +0000

    Deploying to gh-pages from @ bluerobotics/cockpit@c6c4ace4ba72d4529a054a4d7f3c237d1f0743d4 🚀

--- electron/preload.js
+++ electron/preload.js
@@ -1,1 +1,1 @@
-"use strict";const e=require("electron");e.contextBridge.exposeInMainWorld("electronAPI",{getNetworkInfo:()=>e.ipcRenderer.invoke("get-network-info")});
+"use strict";const e=require("electron");e.contextBridge.exposeInMainWorld("electronAPI",{getInfoOnSubnets:()=>e.ipcRenderer.invoke("get-info-on-subnets")});
