627ac6933f37e96e53dfda4b61c7b36a388fc66c
Arturo Manzoli <arturomanzoli@gmail.com>
2025-02-12 10:56:10 -0300

    Components: Snackbar-container: Add stackable snackbar element and implement its use on related files
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

---
+++ src/components/SnackbarContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="snackbar-container" class="snackbar-container">
+    <Snackbar v-for="snack in snackbars" :key="snack.id" v-bind="snack" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Snackbar from '@/components/Snackbar.vue'
+import { useSnackbar } from '@/composables/snackbar'
+
+const { snackbars } = useSnackbar()
+</script>
+
+<style scoped>
+.snackbar-container {
+  position: fixed;
+  bottom: 97px;
+  left: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+  z-index: 9999;
+}
+</style>
