6da7db2024b93c1540149166fe69eda43ed9a2fe
Arturo Manzoli <arturomanzoli@gmail.com>
2025-02-14 16:16:12 -0300

    Mini-widgets: Go-full-screen: Add mini widget
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

---
+++ src/components/mini-widgets/GoFullScreen.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-tooltip :text="isFullScreen ? 'Exit full screen' : 'Go full screen'" location="bottom">
+      <template #activator="{ props: tooltipProps }">
+        <div v-bind="tooltipProps" class="relative cursor-pointer" @click="toggleFullScreen">
+          <FontAwesomeIcon :icon="fullScreenToggleIcon" size="xl text-white" />
+        </div>
+      </template>
+    </v-tooltip>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useFullscreen } from '@vueuse/core'
+import { computed } from 'vue'
+
+const { isFullscreen, toggle: toggleFullscreen } = useFullscreen()
+
+const fullScreenToggleIcon = computed(() => (isFullscreen.value ? 'fa-solid fa-compress' : 'fa-solid fa-expand'))
+const isFullScreen = computed(() => isFullscreen.value)
+
+const toggleFullScreen = (): void => {
+  toggleFullscreen()
+}
+</script>
