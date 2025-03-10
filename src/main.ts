8df421ee6d8b038311ac33f438704dee14fe81be
Arturo Manzoli <arturomanzoli@gmail.com>
2025-03-10 06:41:51 -0300

    App: Add context menu directive and its implementation
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ import eventTracker from '@/libs/external-telemetry/event-tracking'
 import { runMigrations } from '@/utils/migrations'
 
 import App from './App.vue'
+import { contextMenu } from './directives/contextMenu'
 import { setupPredefinedLakeAndActionResources } from './libs/joystick/protocols/predefined-resources'
 import { setupPostPiniaConnections } from './libs/post-pinia-connections'
 import vuetify from './plugins/vuetify'
@@ -55,6 +56,7 @@ if (window.localStorage.getItem('cockpit-enable-usage-statistics-telemetry') && 
 
 app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.component('VueDraggableResizable', VueDraggableResizable)
+app.directive('contextmenu', contextMenu)
 app.use(router).use(vuetify).use(createPinia()).use(FloatingVue).use(VueVirtualScroller)
 app.mount('#app')
 
