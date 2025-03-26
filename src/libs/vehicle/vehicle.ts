718c5f42dd8bf02d819f331a5dd68bb2fc881992
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-03-26 15:44:12 -0300

    generic-variables-system: Deprecate it and migrate everything to the data-lake system

--- src/libs/vehicle/vehicle.ts
+++ src/libs/vehicle/vehicle.ts
@@ -66,10 +66,6 @@ export abstract class AbstractVehicle<Modes> {
   _cockpitRegistrationUUID: string
   _funnyName: string
 
-  // Used to store generic data that does not belong to any specific subclass
-  // Usually used for development purposes (e.g.: test the implementation of new sensors)
-  _genericVariables: Record<string, unknown> = {}
-
   // Signals
   onArm = new Signal<boolean>()
   onAttitude = new Signal<Attitude>()
@@ -77,7 +73,6 @@ export abstract class AbstractVehicle<Modes> {
   onBatteries = new Signal<Battery[]>()
   onCpuLoad = new Signal<number>()
   onCommandAck = new Signal<CommandAck>()
-  onGenericVariables = new Signal<Record<string, unknown>>()
   onMode = new Signal<Modes>()
   onPosition = new Signal<Coordinates>()
   onPowerSupply = new Signal<PowerSupply>()
@@ -113,7 +108,6 @@ export abstract class AbstractVehicle<Modes> {
     this.onAttitude.register_caller(() => this.attitude())
     this.onBatteries.register_caller(() => this.batteries())
     this.onCpuLoad.register_caller(() => this.cpuLoad())
-    this.onGenericVariables.register_caller(() => this.genericVariables())
     this.onMode.register_caller(() => this.mode())
     this.onPosition.register_caller(() => this.position())
     this.onPowerSupply.register_caller(() => this.powerSupply())
@@ -138,14 +132,6 @@ export abstract class AbstractVehicle<Modes> {
    */
   type(): Type {
     return this._type
-  }
-
-  /**
-   * Return the generic variables hashmap of the vehicle
-   * @returns {Record<string, unknown>}
-   */
-  genericVariables(): Record<string, unknown> {
-    return this._genericVariables
   }
 
   /**
