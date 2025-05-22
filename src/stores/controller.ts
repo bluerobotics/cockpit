e2cc11baa4ca5a3e63bc505227a8aea123493a48
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-05-22 16:47:23 -0300

    controller: Disable forwarding by default if there are other GCSs controlling the vehicle already

--- src/stores/controller.ts
+++ src/stores/controller.ts
@@ -12,6 +12,7 @@ import {
 } from '@/assets/joystick-profiles'
 import { useInteractionDialog } from '@/composables/interactionDialog'
 import { useBlueOsStorage } from '@/composables/settingsSyncer'
+import { checkForOtherManualControlSources } from '@/libs/blueos'
 import { MavType } from '@/libs/connection/m2r/messages/mavlink2rest-enum'
 import { joystickCalibrationOptionsKey, joystickManager, JoystickModel, JoysticksMap } from '@/libs/joystick/manager'
 import { allAvailableAxes, allAvailableButtons } from '@/libs/joystick/protocols'
@@ -19,6 +20,7 @@ import { CockpitActionsFunction, executeActionCallback } from '@/libs/joystick/p
 import { modifierKeyActions, otherAvailableActions } from '@/libs/joystick/protocols/other'
 import { Alert, AlertLevel } from '@/types/alert'
 import {
+  type GamepadToCockpitStdMapping,
   type JoystickProtocolActionsMapping,
   type JoystickState,
   type ProtocolAction,
@@ -50,7 +52,10 @@ export const useControllerStore = defineStore('controller', () => {
   const updateCallbacks = ref<controllerUpdateCallback[]>([])
   const protocolMappings = useBlueOsStorage(protocolMappingsKey, cockpitStandardToProtocols)
   const protocolMappingIndex = useBlueOsStorage(protocolMappingIndexKey, 0)
-  const userCustomCockpitStdMappings = useBlueOsStorage(cockpitStdMappingsKey, {})
+  const userCustomCockpitStdMappings = useBlueOsStorage<{ [key in JoystickModel]?: GamepadToCockpitStdMapping }>(
+    cockpitStdMappingsKey,
+    {}
+  )
   const availableAxesActions = ref(allAvailableAxes())
   const availableButtonActions = ref(allAvailableButtons())
   const enableForwarding = ref(false)
@@ -154,8 +159,10 @@ export const useControllerStore = defineStore('controller', () => {
     processJoystickStateEvent(gamepadIndex, currentState, gamepad)
   )
 
-  const processJoystickConnectionEvent = (event: JoysticksMap): void => {
+  const processJoystickConnectionEvent = async (event: JoysticksMap): Promise<void> => {
     const newMap = new Map(Array.from(event).map(([index, gamepad]) => [index, new Joystick(gamepad)]))
+
+    const thereWereJoysticksBefore = joysticks.value.size > 0
 
     // Add new joysticks
     for (const [index, joystick] of newMap) {
@@ -164,8 +171,35 @@ export const useControllerStore = defineStore('controller', () => {
       const { product_id, vendor_id } = joystickManager.getVidPid(joystick.gamepad)
       joysticks.value.set(index, joystick)
       console.info(`Joystick ${index} connected. Model: ${joystick.model} // VID: ${vendor_id} // PID: ${product_id}`)
-      console.info('Enabling joystick forwarding.')
-      enableForwarding.value = true
+
+      if (thereWereJoysticksBefore && enableForwarding.value) {
+        console.warn('There are joysticks connected and forwarding already. Skipping joystick conflict check.')
+        return
+      }
+
+      // Check if other GCS is sending MANUAL_CONTROL messages
+      const otherSourceDetected = await checkForOtherManualControlSources()
+
+      if (otherSourceDetected) {
+        console.warn('Other GCS sending MANUAL_CONTROL messages detected. Disabling joystick forwarding.')
+        enableForwarding.value = false
+
+        showDialog({
+          title: 'Multiple joystick controllers detected',
+          message: [
+            `Another ground control station is already sending joystick commands to this vehicle, and using multiple
+            joysticks simultaneously can cause unpredictable behavior.`,
+            `If you still want to use this joystick, click the top-right joystick widget and enable forwarding. You can
+            also disable the joystick forwarding on the other Cockpit instance the same way.`,
+          ],
+          variant: 'warning',
+          maxWidth: 720,
+          persistent: false,
+        })
+      } else {
+        console.info('No other sources of joystick commands detected. Enabling joystick forwarding.')
+        enableForwarding.value = true
+      }
     }
 
     // Remove joysticks that doesn't not exist anymore
@@ -503,5 +537,6 @@ export const useControllerStore = defineStore('controller', () => {
     joystickCalibrationOptions,
     currentMainJoystick,
     disabledJoysticks,
+    checkForOtherManualControlSources,
   }
 })
