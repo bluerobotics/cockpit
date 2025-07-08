40cdc0dee557a1eb86b136d6012b0e74ae0dd1aa
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-07-08 16:58:05 -0300

    electron: Generate a separated log for each session

--- src/electron/services/electron-log.ts
+++ src/electron/services/electron-log.ts
@@ -1,3 +1,4 @@
+import { format } from 'date-fns'
 import { ipcMain } from 'electron'
 import logger from 'electron-log'
 
@@ -5,6 +6,12 @@ import logger from 'electron-log'
  * Setup electron-log service for system logging
  */
 export const setupElectronLogService = (): void => {
+  // Configure file transport to create a new log file for each session
+  logger.transports.file.fileName = `Cockpit (${format(new Date(), systemLogDateTimeFormat)}).syslog`
+  logger.transports.file.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}'
+  logger.transports.file.maxSize = 10 * 1024 * 1024 // 10MB max file size
+  logger.transports.file.archiveLog = (file) => file + '.old' // Archive old logs
+
   // Set up system logging IPC handler
   ipcMain.on('system-log', (_event, { level, message }) => {
     switch (level) {
