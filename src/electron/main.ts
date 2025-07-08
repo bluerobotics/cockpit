25c089834842d7d1afdf7b82df46f3e3bea32a35
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-07-08 16:42:04 -0300

    electron: Merge internal logs to `electron-log` ones

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -4,6 +4,7 @@ import { join } from 'path'
 
 import { setupAutoUpdater } from './services/auto-update'
 import store from './services/config-store'
+import { setupElectronLogService } from './services/electron-log'
 import { setupJoystickMonitoring } from './services/joystick'
 import { setupNetworkService } from './services/network'
 import { setupResourceMonitoringService } from './services/resource-monitoring'
@@ -87,6 +88,7 @@ setupNetworkService()
 setupResourceMonitoringService()
 setupWorkspaceService()
 setupJoystickMonitoring()
+setupElectronLogService()
 
 app.whenReady().then(async () => {
   console.log('Electron app is ready.')
