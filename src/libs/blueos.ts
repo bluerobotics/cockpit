0df6a3d6d3b0e3bcb4bcedbb9f0c4f69674e8fbc
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-08-04 18:03:07 -0300

    blueos: Fix type of `getKeyData` method

--- src/libs/blueos.ts
+++ src/libs/blueos.ts
@@ -102,7 +102,7 @@ export const getBagOfHoldingFromVehicle = async (
 export const getKeyDataFromCockpitVehicleStorage = async (
   vehicleAddress: string,
   storageKey: string
-): Promise<Record<string, any> | undefined> => {
+): Promise<any | undefined> => {
   return await getBagOfHoldingFromVehicle(vehicleAddress, `cockpit/${storageKey}`)
 }
 
