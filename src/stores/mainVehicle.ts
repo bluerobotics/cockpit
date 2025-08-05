f960dff148eb06222264cc6c06cb61dd13a97bd2
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-08-04 22:34:51 -0300

    blueos: Remove Vue-related dependencies in the lib
    This allows `blueos.ts` to be used outside the Vue application (like in the settings management system, which starts before Vue).

--- src/stores/mainVehicle.ts
+++ src/stores/mainVehicle.ts
@@ -448,6 +448,20 @@ export const useMainVehicleStore = defineStore('main-vehicle', () => {
     }
   }
 
+  /**
+   * Get vehicle address. Waits until the vehicle address is available.
+   * @returns {Promise<string>} The vehicle address
+   */
+  async function getVehicleAddress(): Promise<string> {
+    // Wait until we have a global address
+    while (globalAddress.value === undefined) {
+      console.debug('Waiting for vehicle global address to be available...')
+      await new Promise((r) => setTimeout(r, 1000))
+    }
+
+    return globalAddress.value
+  }
+
   ConnectionManager.onMainConnection.add(() => {
     const newMainConnection = ConnectionManager.mainConnection()
     console.log('Main connection changed:', newMainConnection?.uri().toString())
@@ -897,5 +911,6 @@ export const useMainVehicleStore = defineStore('main-vehicle', () => {
     resetMessageIntervalsToCockpitDefault,
     fetchHomeWaypoint,
     setHomeWaypoint,
+    getVehicleAddress,
   }
 })
