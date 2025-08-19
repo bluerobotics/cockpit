404bb25e90bc6a28153ef5e6f05e820357a17b06
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-08-19 13:30:29 -0300

    ardupilot: Use explicit message to clear missions in the vehicle

--- src/libs/vehicle/ardupilot/ardupilot.ts
+++ src/libs/vehicle/ardupilot/ardupilot.ts
@@ -1185,7 +1185,13 @@ export abstract class ArduPilotVehicle<Modes> extends Vehicle.AbstractVehicle<Mo
    * Clear mission that is on the vehicle
    */
   async clearMissions(): Promise<void> {
-    await this.uploadMission([])
+    const message: Message.MissionClearAll = {
+      type: MAVLinkType.MISSION_CLEAR_ALL,
+      target_system: this.currentSystemId,
+      target_component: 1,
+      mission_type: { type: MavMissionType.MAV_MISSION_TYPE_MISSION },
+    }
+    sendMavlinkMessage(message)
   }
 
   /**
