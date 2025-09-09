b3ce45e098d441ecec28868ae956ee14e4653368
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-09-09 15:48:43 +0200

    blueos: Add alerts for CPU usage and temperature

--- src/stores/vehicleAlerter.ts
+++ src/stores/vehicleAlerter.ts
@@ -1,6 +1,9 @@
 import { defineStore } from 'pinia'
 import { watch } from 'vue'
 
+import { useBlueOsStorage } from '@/composables/settingsSyncer'
+import { listenDataLakeVariable } from '@/libs/actions/data-lake'
+import { blueOsVariables } from '@/libs/blueos/definitions'
 import { useAlertStore } from '@/stores/alert'
 import { useMainVehicleStore } from '@/stores/mainVehicle'
 import { Alert, AlertLevel } from '@/types/alert'
@@ -35,4 +38,34 @@ export const useVehicleAlerterStore = defineStore('vehicle-alerter', () => {
       alertStore.pushAlert(new Alert(alertLevel, `Vehicle ${alertMessage}`))
     }
   )
+
+  // BlueOS alerts
+  const blueOsAlertsConfig = useBlueOsStorage('cockpit-blueos-alerts-config', {
+    'cpu-temperature': { enabled: true, minInterval: 60000, threshold: 85, level: AlertLevel.Warning },
+    'cpu-usage': { enabled: true, minInterval: 60000, threshold: 80, level: AlertLevel.Warning },
+  })
+
+  // BlueOS CPU Temperature Alert
+  let lastBlueOsCpuTemperatureAlertTimestamp = 0
+  listenDataLakeVariable(blueOsVariables.cpuTemp.id, (data) => {
+    const tempConfig = blueOsAlertsConfig.value['cpu-temperature']
+    const thresholdExceeded = Number(data) > tempConfig.threshold
+    const minIntervalReached = Date.now() - lastBlueOsCpuTemperatureAlertTimestamp >= tempConfig.minInterval
+    if (tempConfig.enabled && thresholdExceeded && minIntervalReached) {
+      alertStore.pushAlert(new Alert(tempConfig.level, `BlueOS CPU Temperature threshold exceeded: ${data} °C`))
+      lastBlueOsCpuTemperatureAlertTimestamp = Date.now()
+    }
+  })
+
+  // BlueOS CPU Usage Alert
+  let lastBlueOsCpuUsageAlertTimestamp = 0
+  listenDataLakeVariable(blueOsVariables.cpuUsageAverage.id, (data) => {
+    const usageConfig = blueOsAlertsConfig.value['cpu-usage']
+    const thresholdExceeded = Number(data) > usageConfig.threshold
+    const minIntervalReached = Date.now() - lastBlueOsCpuUsageAlertTimestamp >= usageConfig.minInterval
+    if (usageConfig.enabled && thresholdExceeded && minIntervalReached) {
+      alertStore.pushAlert(new Alert(usageConfig.level, `BlueOS CPU Usage threshold exceeded: ${data} %`))
+      lastBlueOsCpuUsageAlertTimestamp = Date.now()
+    }
+  })
 })
