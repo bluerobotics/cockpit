437e7070be896f6b09190541d73002ac65334c78
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-11-05 10:53:45 -0300

    actions: Enforce type on config

--- src/libs/joystick/protocols/cockpit-actions.ts
+++ src/libs/joystick/protocols/cockpit-actions.ts
@@ -1,8 +1,11 @@
 /* eslint-disable vue/max-len */
 /* eslint-disable prettier/prettier */
 /* eslint-disable max-len */
+import { JavascriptActionConfig } from '@/libs/actions/free-javascript'
+import { HttpRequestActionConfig } from '@/libs/actions/http-request'
+import { MavlinkMessageActionConfig } from '@/libs/actions/mavlink-message-actions'
 import { type ArduPilotVehicleModeActionIdPattern } from '@/libs/vehicle/ardupilot/common'
-import { type ProtocolAction, JoystickProtocol } from '@/types/joystick'
+import { type ProtocolAction,JoystickProtocol } from '@/types/joystick'
 
 /**
  * Possible functions in the MAVLink `MANUAL_CONTROL` message protocol
@@ -165,7 +168,7 @@ export interface ActionConfig {
    * Action configuration
    * Specific to the action type
    */
-  config: any
+  config: HttpRequestActionConfig | MavlinkMessageActionConfig | JavascriptActionConfig
 }
 
 /**
