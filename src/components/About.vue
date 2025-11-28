d026d718f1b4a25afa75be12b035db7d3dea041d
Arturo Manzoli <arturomanzoli@gmail.com>
2025-11-28 15:13:46 -0300

    main-app: add lite version marking to splash-screen and about-dialog
    Signed-off-by: Arturo Manzoli <arturomanzoli@gmail.com>

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,15 @@
       <template #content>
         <div class="flex absolute top-0 right-0"><v-btn icon="mdi-close" variant="text" @click="closeDialog" /></div>
         <div class="flex flex-col justify-center align-center w-full h-full">
-          <img :src="CockpitLogo" alt="Cockpit Logo" class="w-64 my-4" />
+          <div class="relative">
+            <img :src="CockpitLogo" alt="Cockpit Logo" class="w-64 my-4" />
+            <img
+              v-if="!isElectron()"
+              :src="lite"
+              alt="Cockpit lite"
+              class="absolute w-24 right-4 bottom-5 rotate-[-20deg] my-4"
+            />
+          </div>
           <div class="w-[90%] flex justify-between my-6 py-3">
             <div class="w-[45%] flex flex-col text-start">
               <p class="mb-1">
@@ -69,8 +77,10 @@
 import { onUnmounted, ref, watch } from 'vue'
 
 import CockpitLogo from '@/assets/cockpit-logo.png'
+import lite from '@/assets/lite.png'
 import InteractionDialog from '@/components/InteractionDialog.vue'
 import { app_version } from '@/libs/cosmos'
+import { isElectron } from '@/libs/utils'
 
 const showDialog = ref(true)
 const emit = defineEmits(['update:showAboutDialog'])
