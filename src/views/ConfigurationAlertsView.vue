61683e03b1c3ff2ae085dcc2d9207d28d33ba54a
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-12-09 10:59:04 -0300

    alerts: Improve the UI of the config menu

--- src/views/ConfigurationAlertsView.vue
+++ src/views/ConfigurationAlertsView.vue
@@ -4,17 +4,33 @@
     <template #content>
       <div
         class="flex flex-col justify-around align-start ml-5 max-h-[85vh] overflow-y-auto"
-        :class="interfaceStore.isOnSmallScreen ? 'max-w-[70vw]' : 'max-w-[40vw]'"
+        :class="interfaceStore.isOnSmallScreen ? 'max-w-[80vw]' : 'max-w-[50vw]'"
       >
         <ExpansiblePanel :is-expanded="!interfaceStore.isOnPhoneScreen" no-top-divider>
-          <template #title>Voice alerts:</template>
+          <template #title>Text to speech:</template>
           <template #info>
-            Enable voice alerts to receive audible notifications about system and vehicle activities. <br />
-            Select specific alert levels to customize which types of notifications you receive.
+            Enable text to speech to receive audible notifications of the system alerts. <br />
+            Select specific alert levels to customize which ones will be spoken.
           </template>
           <template #content>
-            <div class="flex justify-between">
-              <v-switch v-model="alertStore.enableVoiceAlerts" label="Enable voice alerts" color="white" class="ml-3" />
+            <div class="flex flex-row items-center mb-4 ml-2 gap-x-12">
+              <v-switch
+                v-model="alertStore.enableVoiceAlerts"
+                label="Enable"
+                color="white"
+                hide-details
+                class="min-w-[100px]"
+              />
+              <div class="flex flex-row items-center">
+                <span class="text-sm font-medium">Voice:</span>
+                <Dropdown
+                  v-model="alertStore.selectedAlertSpeechVoiceName"
+                  :options="alertStore.availableAlertSpeechVoiceNames"
+                  name-key="name"
+                  value-key="value"
+                  class="min-w-[250px] ml-3"
+                />
+              </div>
               <v-slider
                 v-model="alertStore.alertVolume"
                 min="0"
@@ -23,11 +39,11 @@
                 hide-details
                 label="Alerts volume"
                 color="white"
-                class="max-w-[300px]"
+                class="min-w-[200px]"
                 :disabled="!alertStore.enableVoiceAlerts"
               />
             </div>
-            <span class="text-sm font-medium mt-4">Alert levels:</span>
+            <span class="text-sm font-medium mt-4">Levels to speak:</span>
             <div class="flex flex-wrap items-center justify-start">
               <div
                 v-for="enabledLevel in alertStore.enabledAlertLevels"
@@ -42,14 +58,6 @@
                 />
               </div>
             </div>
-            <span class="text-sm font-medium mt-4">Alert voice:</span>
-            <Dropdown
-              v-model="alertStore.selectedAlertSpeechVoiceName"
-              :options="alertStore.availableAlertSpeechVoiceNames"
-              name-key="name"
-              value-key="value"
-              class="max-w-[350px] mt-2 mb-4 ml-2"
-            />
           </template>
         </ExpansiblePanel>
         <!-- Armed Menu Warning Toggle -->
