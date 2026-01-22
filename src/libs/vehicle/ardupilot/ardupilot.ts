8c71b615d6321f99654e1b640a957c1c7ee9448a
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2026-01-22 18:56:22 -0300

    feat: Create vehicle-mode-change actions for all ArduPilot vehicles

--- src/libs/vehicle/ardupilot/ardupilot.ts
+++ src/libs/vehicle/ardupilot/ardupilot.ts
@@ -2,6 +2,7 @@ import type { MetadataFile } from '@/types/ardupilot-metadata'
 
 import * as MAVLinkVehicle from '../mavlink/vehicle'
 import * as Vehicle from '../vehicle'
+import { registerModeActions } from './common'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ArduPilot = ArduPilotVehicle<any>
@@ -16,9 +17,11 @@ export abstract class ArduPilotVehicle<Modes> extends MAVLinkVehicle.MAVLinkVehi
    * Construct a new generic ArduPilot type
    * @param {Vehicle.Type} type
    * @param {number} systemId
+   * @param {object} modeEnum - The CustomMode enum for this vehicle type
    */
-  constructor(type: Vehicle.Type, systemId: number) {
+  constructor(type: Vehicle.Type, systemId: number, modeEnum: Record<string, string | number>) {
     super(Vehicle.Firmware.ArduPilot, type, systemId)
+    registerModeActions(type, modeEnum, this)
   }
 
   /**
