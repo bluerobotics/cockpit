rafaellehmkuhl/cockpit-public

blueos: Extend external widget API to include extra attributes

src/components/EditMenu.vue
@@ -870,6 +870,9 @@ const allAvailableWidgets = computed(() => {
       isExternal: true,
       options: {
         source: widget.iframeUrl,
+        containerName: widget.collapsedContainerName,
+        startCollapsed: widget.startCollapsed,
+        useVehicleAddressAsBase: widget.useVehicleAddressAsBaseUrl,
       },
     })),
     ...availableInternalWidgets.value.map((widget) => ({

src/libs/blueos/schemas.ts
@@ -48,6 +48,10 @@ const ExternalWidgetSetupInfoSchema = z.object({
   name: z.string(),
   iframeUrl: z.string(),
   iframeIcon: z.string(),
+  collapsedContainerName: z.string().optional(),
+  version: z.string().optional(),
+  startCollapsed: z.boolean().optional(),
+  useVehicleAddressAsBaseUrl: z.boolean().optional(),
 })
 
 const HttpRequestActionConfigSchema = z.object({

src/types/widgets.ts
@@ -20,6 +20,26 @@ export interface ExternalWidgetSetupInfo {
    * The icon of the widget, this is displayed on the widget browser
    */
   iframeIcon: string
+
+  /**
+   * The name of the collapsed container, this is displayed on the widget browser
+   */
+  collapsedContainerName?: string
+
+  /**
+   * Version of the widget (optional)
+   */
+  version?: string
+
+  /**
+   * Whether the widget should start collapsed (optional)
+   */
+  startCollapsed?: boolean
+
+  /**
+   * Whether to use vehicle address as base URL for the widget (optional)
+   */
+  useVehicleAddressAsBaseUrl?: boolean
 }
 
 /**

bluerobotics/cockpit

src/components/EditMenu.vue
@@ -879,6 +879,9 @@ const allAvailableWidgets = computed(() => {
       isExternal: true,
       options: {
         source: widget.iframeUrl,
+        containerName: widget.collapsibleContainerName,
+        startCollapsed: widget.startCollapsed,
+        useVehicleAddressAsBase: widget.useVehicleAddressAsBaseUrl,
       },
       defaultSize: widgetDefaultSizes[WidgetType.IFrame],
     })),

src/libs/blueos/schemas.ts
@@ -48,6 +48,10 @@ const ExternalWidgetSetupInfoSchema = z.object({
   name: z.string(),
   iframeUrl: z.string(),
   iframeIcon: z.string(),
+  collapsibleContainerName: z.string().optional(),
+  version: z.string().optional(),
+  startCollapsed: z.boolean().optional(),
+  useVehicleAddressAsBaseUrl: z.boolean().optional(),
 })
 
 const HttpRequestActionConfigSchema = z.object({

src/types/widgets.ts
@@ -21,6 +21,26 @@ export interface ExternalWidgetSetupInfo {
    * The icon of the widget, this is displayed on the widget browser
    */
   iframeIcon: string
+
+  /**
+   * The name of the collapsed container, this is displayed on the widget browser
+   */
+  collapsibleContainerName?: string
+
+  /**
+   * Version of the widget (optional)
+   */
+  version?: string
+
+  /**
+   * Whether the widget should start collapsed (optional)
+   */
+  startCollapsed?: boolean
+
+  /**
+   * Whether to use vehicle address as base URL for the widget (optional)
+   */
+  useVehicleAddressAsBaseUrl?: boolean
 }
 
 /**