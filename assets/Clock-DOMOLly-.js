a0aaaa5befe47d579246a81a7042c06dd4b24b96
rafaellehmkuhl <rafael.lehmkuhl93@gmail.com>
2026-03-09 16:29:06 +0000

    Deploying to gh-pages from @ bluerobotics/cockpit@ab30756b1ae9791661cd6b2cac6576fcdf2cd187 🚀

--- assets/Clock-DOMOLly-.js
+++
@@ -1,1 +0,0 @@
-import{c as s,f as n,p as o,q as a,D as c,C as e,G as m}from"./index-StnhJk35.js";const r={class:"flex items-center justify-center m-2 text-sm font-bold text-center text-white select-none min-w-[90px]"},_=s({__name:"Clock",setup(i){const t=n({interval:1e3});return(l,p)=>(a(),o("div",r,c(e(m)(e(t),"E LLL do HH:mm")),1))}});export{_ as default};
