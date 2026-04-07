3057e4054511fd8d9ce220c41de231469334eba2
rafaellehmkuhl <rafael.lehmkuhl93@gmail.com>
2026-04-07 17:12:48 +0000

    Deploying to gh-pages from @ bluerobotics/cockpit@bcb0333566ec42cd2da5bdf11d6589ba18b9e002 🚀

---
+++ assets/Clock-CMjyJXN3.js
@@ -0,0 +1,1 @@
+import{c as s,f as n,q as o,s as a,C as c,B as e,G as m}from"./index-DvcHzpol.js";const r={class:"flex items-center justify-center m-2 text-sm font-bold text-center text-white select-none min-w-[90px]"},_=s({__name:"Clock",setup(i){const t=n({interval:1e3});return(l,f)=>(a(),o("div",r,c(e(m)(e(t),"E LLL do HH:mm")),1))}});export{_ as default};
