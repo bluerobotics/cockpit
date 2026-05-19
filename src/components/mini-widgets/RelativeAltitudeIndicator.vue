ee2af916e5a6d0ff368176cb2313831083c18089
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2026-05-19 10:26:37 -0300

    refactor: migrate RelativeAltitudeIndicator mini-widget to the data-lake
    Replaces the vehicle store altitude watch with useDataLakeVariable
    composable. The altitude source variable ID is now configurable via
    miniWidget.options.altitudeVariableId (default: GLOBAL_POSITION_INT/relative_alt).
    Properly accepts MiniWidget props. Removes useMainVehicleStore dependency.

--- src/components/mini-widgets/RelativeAltitudeIndicator.vue
+++ src/components/mini-widgets/RelativeAltitudeIndicator.vue
@@ -3,8 +3,10 @@
     <img src="@/assets/altitude-icon.svg" class="h-full" :draggable="false" />
     <div class="flex flex-col items-start justify-center ml-1 min-w-[4rem] max-w-[6rem] select-none">
       <div>
-        <span class="font-mono text-xl font-semibold leading-6 w-fit">{{ round(altitude, 2).toFixed(2) }}</span>
-        <span class="text-xl font-semibold leading-6 w-fit"> m</span>
+        <span class="font-mono text-xl font-semibold leading-6 w-fit">{{ parsedState }}</span>
+        <span class="text-xl font-semibold leading-6 w-fit">
+          {{ String.fromCharCode(0x20) }} {{ unitAbbreviation[displayUnitPreferences.distance] }}
+        </span>
       </div>
       <span class="w-full text-sm font-semibold leading-4 whitespace-nowrap">Alt (Rel)</span>
     </div>
@@ -12,13 +14,46 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { unit } from 'mathjs'
+import { computed, onBeforeMount, ref, toRefs, watch } from 'vue'
 
-import { round } from '@/libs/utils'
-import { useMainVehicleStore } from '@/stores/mainVehicle'
+import { useDataLakeVariable } from '@/composables/useDataLakeVariable'
+import { unitAbbreviation } from '@/libs/units'
+import { useAppInterfaceStore } from '@/stores/appInterface'
+import type { MiniWidget } from '@/types/widgets'
 
-const store = useMainVehicleStore()
+const props = defineProps<{
+  /**
+   * Mini widget reference
+   */
+  miniWidget: MiniWidget
+}>()
+const miniWidget = toRefs(props).miniWidget
 
-const altitude = ref(0)
-watch(store.altitude, () => (altitude.value = store.altitude.rel))
+const defaultOptions = {
+  altitudeVariableId: '/mavlink/1/1/GLOBAL_POSITION_INT/relative_alt',
+}
+
+onBeforeMount(() => {
+  miniWidget.value.options = { ...defaultOptions, ...miniWidget.value.options }
+})
+
+const { displayUnitPreferences } = useAppInterfaceStore()
+
+const { value: rawAltitude } = useDataLakeVariable(() => miniWidget.value.options.altitudeVariableId)
+
+const currentAltitude = ref<undefined | number>(undefined)
+watch(rawAltitude, (newAlt) => {
+  if (newAlt === undefined) return
+  const altMeters = (newAlt as number) / 1000
+  const altitude = unit(altMeters, 'm')
+  const altitudeConverted = altitude.to(displayUnitPreferences.distance)
+  currentAltitude.value = altitudeConverted.toJSON().value
+})
+
+const parsedState = computed(() => {
+  const altitude = currentAltitude.value
+  if (altitude === undefined) return '--'
+  return altitude.toFixed(2)
+})
 </script>
